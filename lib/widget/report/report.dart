ce067f2db2b1e788d7ad12b1d0689ea29b8d0565
zzq <2428698039@qq.com>
2021-11-20 23:46:39 +0800

    Add: 添加了每日体温上报模块

---
+++ lib/widget/report/report.dart
@@ -0,0 +1,53 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class DailyReportWidget extends StatefulWidget {
+  const DailyReportWidget({Key? key}) : super(key: key);
+
+  @override
+  _DailyReportWidgetState createState() => _DailyReportWidgetState();
+}
+
+class _DailyReportWidgetState extends State<DailyReportWidget> {
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('报不完的体温上报'),
+        actions: [
+          ElevatedButton(
+            onPressed: () async {
+              var controller = await _controller.future;
+              await controller.reload();
+            },
+            child: const Icon(Icons.refresh),
+          )
+        ],
+      ),
+      body: WebView(
+        initialUrl: 'http://xgfy.sit.edu.cn/h5/#',
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (WebViewController webViewController) {
+          _controller.complete(webViewController);
+        },
+        onProgress: (int progress) {
+          print("WebView is loading (progress : $progress%)");
+        },
+        onPageFinished: (String url) async {
+          print('OnPageFinished: $url');
+          var controller = await _controller.future;
+          controller.runJavascript(
+              r"document.querySelector('.flex-direction > uni-view:nth-child(1)').textContent='上海应用技术大学小风筝研发中心'");
+          controller.runJavascript(
+              r"document.querySelector('.flex-direction > uni-view:nth-child(2)').textContent=''");
+        },
+      ),
+    );
+  }
+}
