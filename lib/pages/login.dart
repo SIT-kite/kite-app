af6c4d4940b657a67d91d47ae4f5b3d4a7a92497
Zhangzqs <i@zhangzqs.cn>
2021-12-29 23:59:01 +0800

    Add: 尝试添加一个首页爬虫

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:kite/services/sso/encrypt_util.dart';
-import 'package:kite/services/sso/login.dart';
+import 'package:kite/services/myportal.dart';
+import 'package:kite/services/sso/session.dart';
 import 'package:dio/dio.dart';
-import 'package:kite/services/sso/nonpersistentCookieJar.dart';
-import 'package:kite/services/ocr.dart';
 import 'package:kite/services/sso/utils.dart';
+import 'package:beautiful_soup_dart/beautiful_soup.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -14,42 +13,32 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  String name = '';
+  String id = '';
+  String yx = '';
+  String lastLogin = '';
+  String lastIp = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('登录'),
+        title: const Text('登录'),
       ),
       body: Column(
         children: [
           ElevatedButton(
             onPressed: () async {
-              var dio = Dio();
-              var jar = NonpersistentCookieJar();
-              var username = '';
-              var password = '';
-              var loginTool = Auth(
-                dio,
-                'https://myportal.sit.edu.cn/',
-                jar,
-                username,
-                password,
-              );
+              var username = '学号';
+              var password = '密码';
+              var session = Session(username, password);
 
-              await loginTool.login();
-
-              // 打开OA首页
-              var res = await dio.get('https://myportal.sit.edu.cn/',
-                  options: DioUtils.NON_REDIRECT_OPTION_WITH_FORM_TYPE);
-
-              // 处理重定向
-              var result = await DioUtils.processRedirect(dio, res);
-
-              // 显示数据
-              print(result);
+              await session.login();
+              var userInfo = await MyPortal(session).getUserInfo();
+              print(userInfo);
             },
-            child: Text('Button'),
-          )
+            child: const Text('Button'),
+          ),
         ],
       ),
     );
---
+++ lib/services/myportal.dart
@@ -0,0 +1,18 @@
+import 'package:kite/services/sso/session.dart';
+import 'package:beautiful_soup_dart/beautiful_soup.dart';
+
+class MyPortal {
+  Session session;
+
+  MyPortal(this.session);
+
+  getUserInfo() async {
+    var response = await session.get('https://myportal.sit.edu.cn/');
+    String html = response.data;
+    return BeautifulSoup(html)
+        .find('div', class_: 'composer')!
+        .findAll('li')
+        .map((e) => e.text.trim().replaceAll('\n', '').replaceAll(' ', ''))
+        .toList();
+  }
+}
