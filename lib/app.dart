2f28c9ae7ed7065442f963c504a9e1d6669c7551
sunnysab <i@sunnysab.cn>
2021-12-29 13:32:07 +0800

    Add: 添加 app.dart, 添加微软 AppCenter 支持.

---
+++ lib/app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_appcenter_bundle/flutter_appcenter_bundle.dart';
+import 'package:flutter/material.dart';
+
+import 'package:kite/main.dart';
+
+void main() async {
+ 
+    /* Initialize Microsoft AppCenter Analytics */
+  await AppCenter.startAsync(
+    appSecretAndroid: '******',
+    appSecretIOS: '******',
+    enableAnalytics: true, // Defaults to true
+    enableCrashes: true, // Defaults to true
+    enableDistribute: false,
+    usePrivateDistributeTrack: false, // Defaults to false
+    disableAutomaticCheckForUpdate: true, // Defaults to false
+  );
+
+  AppCenter.trackEventAsync('Start', <String, String> {
+    'prop1': 'prop1',
+    'prop2': 'prop2',
+  });
+  
+  runApp(const KiteApp());
+}
