92aa162664f180a3b6f902bc8e300859ac6fae0d
sunnysab <i@sunnysab.cn>
2021-12-29 15:06:01 +0800

    Add: 添加引导页 (无图), 删除不必要的文件.

--- lib/app.dart
+++ lib/app.dart
@@ -4,22 +4,21 @@ import 'package:flutter/material.dart';
 import 'package:kite/main.dart';
 
 void main() async {
- 
-    /* Initialize Microsoft AppCenter Analytics */
-  await AppCenter.startAsync(
-    appSecretAndroid: '******',
-    appSecretIOS: '******',
-    enableAnalytics: true, // Defaults to true
-    enableCrashes: true, // Defaults to true
-    enableDistribute: false,
-    usePrivateDistributeTrack: false, // Defaults to false
-    disableAutomaticCheckForUpdate: true, // Defaults to false
-  );
+  /* Initialize Microsoft AppCenter Analytics */
+  // await AppCenter.startAsync(
+  //   appSecretAndroid: '******',
+  //   appSecretIOS: '******',
+  //   enableAnalytics: true, // Defaults to true
+  //   enableCrashes: true, // Defaults to true
+  //   enableDistribute: false,
+  //   usePrivateDistributeTrack: false, // Defaults to false
+  //   disableAutomaticCheckForUpdate: true, // Defaults to false
+  // );
 
-  AppCenter.trackEventAsync('Start', <String, String> {
-    'prop1': 'prop1',
-    'prop2': 'prop2',
-  });
-  
+  // AppCenter.trackEventAsync('Start', <String, String> {
+  //   'prop1': 'prop1',
+  //   'prop2': 'prop2',
+  // });
+
   runApp(const KiteApp());
 }
