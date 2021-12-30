45986ac94a52f981b72a2d236f6db155cb2273ee
Zhangzqs <i@zhangzqs.cn>
2021-12-30 22:49:45 +0800

    Refactor: 移除常量池中的cookieJar, 改变为session

--- lib/services/constants.dart
+++ lib/services/constants.dart
@@ -1,7 +1,8 @@
 import 'package:cookie_jar/cookie_jar.dart';
+import 'package:kite/services/sso/sso.dart';
 
 class Constants {
-  final cookieJar = PersistCookieJar();
+  final session = Session(jar: PersistCookieJar());
   static final Constants _constants = Constants.internal();
   factory Constants() => _constants;
   Constants.internal();
