6f392ade980a57db6e61c87f2217f2d855033eaf
Zhangzqs <i@zhangzqs.cn>
2021-12-30 21:04:16 +0800

    Fix: test导入报错

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:kite/main.dart';
+import 'package:kite/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
