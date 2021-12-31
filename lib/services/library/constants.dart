e1215498e8abbbe12bb3c729213dc3ee44d3a163
Zhangzqs <i@zhangzqs.cn>
2021-12-31 09:47:18 +0800

    Add: 图书馆各种 url 常量

---
+++ lib/services/library/constants.dart
@@ -0,0 +1,9 @@
+class Constants {
+  static const homeUrl = 'http://210.35.66.106';
+  static const opacUrl = homeUrl + '/opac';
+  static const searchUrl = opacUrl + '/search';
+  static const hotSearchUrl = opacUrl + '/hotsearch';
+  static const bookUrl = opacUrl + '/book';
+  static const bookImageInfoUrl =
+      'https://book-resource.dataesb.com/websearch/metares';
+}
