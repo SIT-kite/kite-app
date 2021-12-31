a4308160ff214fe8b4e48ad6bf518fae8fe99f60
sunnysab <i@sunnysab.cn>
2021-12-31 19:18:13 +0800

    Update: 优化欢迎页/登录页代码和样式.

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:kite/services/myportal.dart';
 import 'package:kite/services/sso/sso.dart';
+
+// Rule of student id.
+RegExp reStudentId = RegExp(r'^((\d{9})|(\d{6}[YGHE\d]\d{3}))$');
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -14,11 +18,17 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
-  // Rule of student id.
-  RegExp reStudentId = RegExp(r'^((\d{9})|(\d{6}[YGHE\d]\d{3}))$');
-  bool isPasswordClear = false;
+  GlobalKey _formKey = GlobalKey<FormState>();
 
-  String? _usernameValidator(String? username) {
+  final TapGestureRecognizer _recognizer = TapGestureRecognizer()
+    ..onTap = onOpenUserLicense;
+
+  // State
+  bool isPasswordClear = false;
+  bool isLicenseAccepted = false;
+  bool isDoingLogin = false;
+
+  static String? _usernameValidator(String? username) {
     if (username != null && username.isNotEmpty) {
       // When user complete his input, check it.
       if (((username.length == 9 || username.length == 10) &&
@@ -37,10 +47,93 @@ class _LoginPageState extends State<LoginPage> {
     super.initState();
   }
 
+  void onClickLogin() {
+    final studentId = _usernameController.text;
+    final password = _passwordController.text;
+  }
+
+  static void onOpenUserLicense() {}
+
+  Widget buildTitleLine() {
+    return Container(
+        alignment: Alignment.centerLeft,
+        child: const Text('欢迎登录',
+            style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)));
+  }
+
+  Widget buildLoginForm() {
+    return Form(
+        autovalidateMode: AutovalidateMode.always,
+        key: _formKey,
+        child: Column(
+          children: [
+            TextFormField(
+              controller: _usernameController,
+              autofocus: true,
+              decoration: const InputDecoration(
+                  labelText: '学号',
+                  hintText: '输入你的学号',
+                  icon: Icon(Icons.person)),
+              validator: _usernameValidator,
+            ),
+            TextFormField(
+                controller: _passwordController,
+                autofocus: true,
+                obscureText: !isPasswordClear,
+                decoration: InputDecoration(
+                    suffixIcon: IconButton(
+                      icon: Icon(isPasswordClear
+                          ? Icons.visibility_off
+                          : Icons.visibility),
+                      onPressed: () {
+                        setState(() {
+                          isPasswordClear = !isPasswordClear;
+                        });
+                      },
+                    ),
+                    labelText: '密码',
+                    hintText: '输入你的密码',
+                    icon: const Icon(Icons.lock))),
+          ],
+        ));
+  }
+
+  Widget buildUserLicenseCheckbox() {
+    return Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Checkbox(
+              value: isLicenseAccepted,
+              onChanged: (_isLicenseAccepted) {
+                setState(() {
+                  isLicenseAccepted = _isLicenseAccepted!;
+                });
+              }),
+          Text.rich(TextSpan(children: [
+            const TextSpan(text: '我已阅读并同意'),
+            TextSpan(
+                text: '《上应小风筝用户协议》',
+                style: const TextStyle(color: Colors.blue),
+                recognizer: _recognizer),
+          ]))
+        ]);
+  }
+
+  Widget buildLoginButton() {
+    return Row(crossAxisAlignment: CrossAxisAlignment.end, children: [
+      SizedBox(
+        height: 40,
+        child: ElevatedButton(
+          onPressed: () {},
+          child: const Text('进入风筝元宇宙'),
+        ),
+      )
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
-    GlobalKey _formKey = new GlobalKey<FormState>();
-
     var screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
@@ -54,62 +147,16 @@ class _LoginPageState extends State<LoginPage> {
         mainAxisSize: MainAxisSize.min,
         children: [
           // Title field.
-          Container(
-              alignment: Alignment.centerLeft,
-              child: const Text('欢迎登录',
-                  style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold))),
+          buildTitleLine(),
           const Padding(padding: EdgeInsets.only(top: 40.0)),
-
           // Form field: username and password.
-          Form(
-              autovalidateMode: AutovalidateMode.always,
-              key: _formKey,
-              child: Column(
-                children: [
-                  TextFormField(
-                    controller: _usernameController,
-                    autofocus: true,
-                    decoration: const InputDecoration(
-                        labelText: '学号',
-                        hintText: '输入你的学号',
-                        icon: Icon(Icons.person)),
-                    validator: _usernameValidator,
-                  ),
-                  TextFormField(
-                      controller: _passwordController,
-                      autofocus: true,
-                      obscureText: !isPasswordClear,
-                      decoration: InputDecoration(
-                          suffixIcon: IconButton(
-                            icon: Icon(isPasswordClear
-                                ? Icons.visibility_off
-                                : Icons.visibility),
-                            onPressed: () {
-                              setState(() {
-                                isPasswordClear = !isPasswordClear;
-                              });
-                            },
-                          ),
-                          labelText: '密码',
-                          hintText: '输入你的密码',
-                          icon: const Icon(Icons.lock))),
-                ],
-              )),
-          const SizedBox(height: 40),
-
+          buildLoginForm(),
+          const SizedBox(height: 10),
+          // User licenese check box.
+          buildUserLicenseCheckbox(),
+          const SizedBox(height: 25),
           // Login button.
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              SizedBox(
-                height: 40,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('进入风筝元宇宙'),
-                ),
-              ),
-            ],
-          )
+          buildLoginButton(),
         ],
       ),
     )));
