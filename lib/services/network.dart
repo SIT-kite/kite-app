03e9e1feee31747ebadc87fc79250e905d5c438c
Zhangzqs <i@zhangzqs.cn>
2022-01-01 00:27:15 +0800

    Update: 新增sit上网登录的实体类

--- lib/services/network.dart
+++ lib/services/network.dart
@@ -1,4 +1,34 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'network.g.dart';
+
+@JsonSerializable()
+class CheckStatusResult {
+  final int result;
+  final int time;
+
+  const CheckStatusResult(this.result, this.time);
+  factory CheckStatusResult.fromJson(Map<String, dynamic> json) =>
+      _$CheckStatusResultFromJson(json);
+  Map<String, dynamic> toJson() => _$CheckStatusResultToJson(this);
+}
+
+@JsonSerializable()
+class LogoutResult {
+  factory LogoutResult.fromJson(Map<String, dynamic> json) =>
+      _$LogoutResultFromJson(json);
+  Map<String, dynamic> toJson() => _$LogoutResultToJson(this);
+}
+
+@JsonSerializable()
+class LoginResult {
+  factory LoginResult.fromJson(Map<String, dynamic> json) =>
+      _$LoginResultFromJson(json);
+  Map<String, dynamic> toJson() => _$LoginResultToJson(this);
+}
 
 class Network {
   static const _indexUrl = 'http://172.16.8.70';
@@ -7,13 +37,21 @@ class Network {
   static const _checkStatusUrl = _drcomUrl + '/chkstatus';
   static const _logoutUrl = _drcomUrl + '/logout';
 
-  static Future<Response> _get(String url,
+  static Future<Map<String, dynamic>> _get(String url,
       {Map<String, dynamic>? queryParameters}) async {
-    return await Dio().get(url, queryParameters: queryParameters);
+    var response = await Dio().get(
+      url,
+      queryParameters: queryParameters,
+      options: Options(
+        responseType: ResponseType.plain,
+      ),
+    );
+    var jsonp = response.data.toString();
+    return jsonDecode(jsonp.substring(7, jsonp.length - 1));
   }
 
-  static Future<Response> login(String username, String password) async {
-    return await _get(
+  static Future<LoginResult> login(String username, String password) async {
+    return LoginResult.fromJson(await _get(
       _loginUrl,
       queryParameters: {
         'callback': 'dr1003',
@@ -29,28 +67,28 @@ class Network {
         'lang': 'zh-cn',
         'jsVersion': '4.1',
       },
-    );
+    ));
   }
 
-  static Future<Response> checkStatus() async {
-    return await _get(
+  static Future<CheckStatusResult> checkStatus() async {
+    return CheckStatusResult.fromJson(await _get(
       _checkStatusUrl,
       queryParameters: {
         'callback': 'dr1002',
         'jsVersion': '4.X',
         'lang': 'zh',
       },
-    );
+    ));
   }
 
-  static Future<Response> logout() async {
-    return await _get(
+  static Future<LogoutResult> logout() async {
+    return LogoutResult.fromJson(await _get(
       _logoutUrl,
       queryParameters: {
         'callback': 'dr1002',
         'jsVersion': '4.1.3',
         'lang': 'zh',
       },
-    );
+    ));
   }
 }
