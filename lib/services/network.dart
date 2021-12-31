3fd2a54884b2c705a5b887c1fe07bd1b815ad02f
Zhangzqs <i@zhangzqs.cn>
2021-12-31 09:46:16 +0800

    Add: SIT上网认证服务

---
+++ lib/services/network.dart
@@ -0,0 +1,56 @@
+import 'package:dio/dio.dart';
+
+class Network {
+  static const _indexUrl = 'http://172.16.8.70';
+  static const _drcomUrl = _indexUrl + '/drcom';
+  static const _loginUrl = _drcomUrl + '/login';
+  static const _checkStatusUrl = _drcomUrl + '/chkstatus';
+  static const _logoutUrl = _drcomUrl + '/logout';
+
+  Future<Response> _get(String url,
+      {Map<String, dynamic>? queryParameters}) async {
+    return await Dio().get(url, queryParameters: queryParameters);
+  }
+
+  Future<Response> login(String username, String password) async {
+    return await _get(
+      _loginUrl,
+      queryParameters: {
+        'callback': 'dr1003',
+        'DDDDD': username,
+        'upass': password,
+        '0MKKey': '123456',
+        "R1'": '0',
+        'R2': '',
+        'R3': '0',
+        'R6': '0',
+        'para': '00',
+        'terminal_type': '1',
+        'lang': 'zh-cn',
+        'jsVersion': '4.1',
+      },
+    );
+  }
+
+  Future<Response> checkStatus() async {
+    return await _get(
+      _checkStatusUrl,
+      queryParameters: {
+        'callback': 'dr1002',
+        'jsVersion': '4.X',
+        'lang': 'zh',
+      },
+    );
+  }
+
+  Future<Response> logout() async {
+    return await _get(
+      _logoutUrl,
+      queryParameters: {
+        'callback': 'dr1002',
+        'jsVersion': '4.1.3',
+        'lang': 'zh',
+      },
+    );
+  }
+}
