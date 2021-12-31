63a04f7ee79c9fdd392664d85e755dc8aa98eeff
Zhangzqs <i@zhangzqs.cn>
2021-12-31 23:28:08 +0800

    Add: 新建馆藏信息爬虫

--- lib/services/library/constants.dart
+++ lib/services/library/constants.dart
@@ -3,7 +3,10 @@ class Constants {
   static const opacUrl = homeUrl + '/opac';
   static const searchUrl = opacUrl + '/search';
   static const hotSearchUrl = opacUrl + '/hotsearch';
+  static const apiUrl = opacUrl + '/api';
   static const bookUrl = opacUrl + '/book';
+  static const bookHoldingUrl = apiUrl + '/holding';
+
   static const bookImageInfoUrl =
       'https://book-resource.dataesb.com/websearch/metares';
 }
