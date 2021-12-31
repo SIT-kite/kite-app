bded8ec12f26924cef1e9b50283260927b4207b9
Zhangzqs <i@zhangzqs.cn>
2021-12-31 19:09:28 +0800

    Add: 完善图书搜索相关代码

--- lib/services/library/search.dart
+++ lib/services/library/search.dart
@@ -1,3 +1,7 @@
+import 'package:beautiful_soup_dart/beautiful_soup.dart';
+import 'package:kite/services/library/constants.dart';
+import 'package:dio/dio.dart';
+
 enum SearchWay {
   // 按任意词查询
   any,
@@ -67,6 +71,28 @@ String sortOrderToString(SortOrder sw) {
   return {SortOrder.asc: 'asc', SortOrder.desc: 'desc'}[sw]!;
 }
 
+class Book {
+  String bookId;
+  String isbn;
+  String title;
+  String author;
+  String publisher;
+  String publishDate;
+  String callNo;
+  Book(this.bookId, this.isbn, this.title, this.author, this.publisher,
+      this.publishDate, this.callNo);
+}
+
+class BookSearchResult {
+  int resultCount;
+  double useTime;
+  int currentPage;
+  int totalPages;
+  List<Book> books;
+  BookSearchResult(this.resultCount, this.useTime, this.currentPage,
+      this.totalPages, this.books);
+}
+
 class SearchLibraryRequest {
   // 搜索关键字
   String keyword;
@@ -89,4 +115,63 @@ class SearchLibraryRequest {
     this.sortWay = SortWay.matchScore,
     this.sortOrder = SortOrder.asc,
   });
+
+  Future<BookSearchResult> request() async {
+    var response = await Dio().get(
+      Constants.searchUrl,
+      queryParameters: {
+        'q': keyword,
+        'searchType': 'standard',
+        'isFacet': 'true',
+        'view': 'standard',
+        'searchWay': searchWayToString(searchWay),
+        'rows': rows.toString(),
+        'sortWay': sortWayToString(sortWay),
+        'sortOrder': sortOrderToString(sortOrder),
+        'hasholding': '1',
+        'searchWay0': 'marc',
+        'logical0': 'AND',
+        'page': page.toString(),
+      },
+    );
+
+    Book parseBook(Bs4Element e) {
+      // 获得图书信息
+      String getBookInfo(String selector) {
+        return e.find(selector)!.text!.trim();
+      }
+
+      var bookCoverImage = e.find('.bookcover_img')!;
+      var author = getBookInfo('.author-link');
+      var bookId = bookCoverImage.attributes['bookrecno']!;
+      var isbn = bookCoverImage.attributes['isbn']!;
+      var callNo = getBookInfo('.callnosSpan');
+      var publishDate =
+          getBookInfo('td:nth-child(4) > div:nth-child(1) > div:nth-child(3)')
+              .split('出版日期:')[1];
+      var publisher = getBookInfo('.publisher-link');
+      var title = getBookInfo('.title-link');
+      return Book(bookId, isbn, title, author, publisher, publishDate, callNo);
+    }
+
+    var htmlElement = BeautifulSoup(response.data);
+
+    var currentPage = 0;
+    var resultNumAndTime =
+        htmlElement.find('#search_meta > div:nth-child(1)')!.text;
+    print(resultNumAndTime);
+    var totalPages =
+        htmlElement.find('div.meneame:nth-child(4) > span:nth-child(1)')!.text;
+    print(totalPages);
+
+    return BookSearchResult(
+        1,
+        0,
+        currentPage,
+        0,
+        htmlElement
+            .findAll('.resultTable > tbody:nth-child(1) > tr')
+            .map((e) => parseBook(e))
+            .toList());
+  }
 }
