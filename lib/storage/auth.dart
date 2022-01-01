59564c5547a28a8ee967a6cecf9693372bab6d92
Zhangzqs <i@zhangzqs.cn>
2022-01-01 11:41:04 +0800

    Add: 新增用户账户认证的持久化层代码

---
+++ lib/storage/auth.dart
@@ -0,0 +1,17 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthStorage {
+  static const _namespace = '/auth/';
+  static const _usernameKey = _namespace + 'username';
+  static const _passwordKey = _namespace + 'password';
+
+  final SharedPreferences prefs;
+  const AuthStorage(this.prefs);
+
+  String get username => prefs.getString(_usernameKey)!;
+  String get password => prefs.getString(_passwordKey)!;
+  set username(String foo) => prefs.setString(_usernameKey, foo);
+  set password(String foo) => prefs.setString(_passwordKey, foo);
+  bool get hasUsername => prefs.containsKey(_usernameKey);
+  bool get hasPassword => prefs.containsKey(_passwordKey);
+}
