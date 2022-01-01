0ed933f9d9eadc89ff5845d6a5c257650eeb332b
sunnysab <i@sunnysab.cn>
2022-01-01 23:05:54 +0800

    Add: 添加网络工具页.

--- lib/pages/connectivity.dart
+++ lib/pages/connectivity.dart
@@ -1,5 +1,9 @@
+import 'package:check_vpn_connection/check_vpn_connection.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_icmp_ping/flutter_icmp_ping.dart';
 import 'package:flutter_svg/svg.dart';
+
+import 'package:kite/services/network.dart';
 
 class ConnectivityPage extends StatefulWidget {
   ConnectivityPage({Key? key}) : super(key: key);
@@ -11,17 +15,72 @@ class ConnectivityPage extends StatefulWidget {
 class _ConnectivityPageState extends State<ConnectivityPage> {
   final TextStyle textStyle = const TextStyle().copyWith(fontSize: 20);
 
-  final Widget connectivityPicture = SvgPicture.asset(
+  final Widget disconnectedPicture = SvgPicture.asset(
       'assets/connectivity/not-available.svg',
       width: 260,
       height: 260,
       color: Colors.grey);
+  final Widget connectedPicture = SvgPicture.asset(
+      'assets/connectivity/not-available.svg',
+      width: 260,
+      height: 260,
+      color: Colors.green[600]);
 
-  Widget buildErrorText() {
-    return Text(
-      '无法连接到校园网。\n请连接 i-SIT, i-SIT-1x, eduroam 中的任意一个热点\n或打开 EasyConnect App 以连接 VPN。',
-      textAlign: TextAlign.center,
-      style: textStyle.copyWith(fontSize: 15),
+  bool isConnected = false;
+
+  bool checkConnectivity() {
+    /*
+    There are 2 situations: Connected to LAN or WLAN to campus network directly, or connect by VPN.
+    210.35.96.2 is a DNS server of inner network, which can be accessed in both situations.
+    */
+    final ping =
+        Ping('210.35.96.2', count: 1, timeout: 2, interval: 1, ipv6: false);
+    bool isSuccess = false;
+
+    ping.stream.listen((event) {
+      if (event.summary?.received != null && event.summary?.received == 1) {
+        isSuccess = true;
+      }
+    });
+    return isSuccess;
+  }
+
+  Widget getFigure() => isConnected ? connectedPicture : disconnectedPicture;
+
+  Widget buildText() {
+    Widget buildErrorText() {
+      return Text(
+        '无法连接到校园网。\n请连接 i-SIT, i-SIT-1x, eduroam 中的任意一个热点\n或打开 EasyConnect App 以连接 VPN。',
+        textAlign: TextAlign.center,
+        style: textStyle.copyWith(fontSize: 15),
+      );
+    }
+
+    Widget buildConnectedByVpnText() => const Text('当前已通过 VPN 连接校园网');
+    Widget buildConnectedByWlan(String ip, String studentId) =>
+        Text('当前已通过 WLAN 登录\n登录地址: $ip\n登录用户: $studentId');
+    return FutureBuilder<bool>(
+      future: CheckVpnConnection.isVpnActive(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          bool isVpnActive = snapshot.data!;
+          if (isVpnActive) {
+            return buildConnectedByVpnText();
+          }
+        }
+        return FutureBuilder<CheckStatusResult>(
+          future: Network.checkStatus(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              CheckStatusResult result = snapshot.data!;
+              if (result.result == 1) {
+                return buildConnectedByWlan(result.ip, result.uid ?? '');
+              }
+            }
+            return buildErrorText();
+          },
+        );
+      },
     );
   }
 
@@ -53,6 +112,17 @@ class _ConnectivityPageState extends State<ConnectivityPage> {
     );
   }
 
+  Widget buildDisconnectedBlock() {
+    return Column(
+        children: [buildText(), const SizedBox(height: 10.0), buildButtons()]);
+  }
+
+  Widget buildConnectedBlock() {
+    return Column(children: [
+      buildText(),
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,11 +131,10 @@ class _ConnectivityPageState extends State<ConnectivityPage> {
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            connectivityPicture,
+            getFigure(),
             const SizedBox(height: 60.0),
-            buildErrorText(),
-            const SizedBox(height: 10.0),
-            buildButtons(),
+            // isConnected ?
+            buildText(),
           ],
         ),
       ),
