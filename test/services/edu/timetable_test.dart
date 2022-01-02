42ae826b4ff1959bf284da606bfdbbd65105349b
Zhangzqs <i@zhangzqs.cn>
2022-01-02 22:53:45 +0800

    Add: 添加课表测试

---
+++ test/services/edu/timetable_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:kite/services/edu/edu.dart';
+import 'package:kite/services/sso/src/session.dart';
+import 'package:logger/logger.dart';
+
+void main() {
+  var logger = Logger();
+  test('timetable test', () async {
+    Session session = Session();
+    await session.login('', '');
+    await session.get('http://jwxt.sit.edu.cn/sso/jziotlogin');
+    var table = await Timetable(session).getTimetable();
+    logger.i(table);
+  });
+}
