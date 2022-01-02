b4e51718df7fff941046a84665038cfb76b849b9
Zhangzqs <i@zhangzqs.cn>
2022-01-02 21:31:38 +0800

    Refactor: 将图书搜索函数迁移到BookSearchResult中的静态函数中

--- lib/services/library/src/book_search.dart
+++ lib/services/library/src/book_search.dart
@@ -131,88 +131,88 @@ class BookSearchResult {
   factory BookSearchResult.fromJson(Map<String, dynamic> json) =>
       _$BookSearchResultFromJson(json);
   Map<String, dynamic> toJson() => _$BookSearchResultToJson(this);
-}
 
-Future<BookSearchResult> searchBook({
-  String keyword = '',
-  int rows = 10,
-  int page = 1,
-  SearchWay searchWay = SearchWay.title,
-  SortWay sortWay = SortWay.matchScore,
-  SortOrder sortOrder = SortOrder.desc,
-  Dio? dio,
-}) async {
-  var response = await (dio ?? Dio()).get(
-    Constants.searchUrl,
-    queryParameters: {
-      'q': keyword,
-      'searchType': 'standard',
-      'isFacet': 'true',
-      'view': 'standard',
-      'searchWay': _searchWayToString(searchWay),
-      'rows': rows.toString(),
-      'sortWay': _sortWayToString(sortWay),
-      'sortOrder': _sortOrderToString(sortOrder),
-      'hasholding': '1',
-      'searchWay0': 'marc',
-      'logical0': 'AND',
-      'page': page.toString(),
-    },
-  );
+  Future<BookSearchResult> search({
+    String keyword = '',
+    int rows = 10,
+    int page = 1,
+    SearchWay searchWay = SearchWay.title,
+    SortWay sortWay = SortWay.matchScore,
+    SortOrder sortOrder = SortOrder.desc,
+    Dio? dio,
+  }) async {
+    var response = await (dio ?? Dio()).get(
+      Constants.searchUrl,
+      queryParameters: {
+        'q': keyword,
+        'searchType': 'standard',
+        'isFacet': 'true',
+        'view': 'standard',
+        'searchWay': _searchWayToString(searchWay),
+        'rows': rows.toString(),
+        'sortWay': _sortWayToString(sortWay),
+        'sortOrder': _sortOrderToString(sortOrder),
+        'hasholding': '1',
+        'searchWay0': 'marc',
+        'logical0': 'AND',
+        'page': page.toString(),
+      },
+    );
 
-  Book parseBook(Bs4Element e) {
-    // 获得图书信息
-    String getBookInfo(String name, String selector) {
-      return e.find(name, selector: selector)!.text.trim();
+    Book parseBook(Bs4Element e) {
+      // 获得图书信息
+      String getBookInfo(String name, String selector) {
+        return e.find(name, selector: selector)!.text.trim();
+      }
+
+      var bookCoverImage = e.find('img', class_: 'bookcover_img')!;
+      var author = getBookInfo('a', '.author-link');
+      var bookId = bookCoverImage.attributes['bookrecno']!;
+      var isbn = bookCoverImage.attributes['isbn']!;
+      var callNo = getBookInfo('span', '.callnosSpan');
+      var publishDate =
+          getBookInfo('div', 'div').split('出版日期:')[1].split('\n')[0].trim();
+
+      var publisher = getBookInfo('a', '.publisher-link');
+      var title = getBookInfo('a', '.title-link');
+      return Book(bookId, isbn, title, author, publisher, publishDate, callNo);
     }
 
-    var bookCoverImage = e.find('img', class_: 'bookcover_img')!;
-    var author = getBookInfo('a', '.author-link');
-    var bookId = bookCoverImage.attributes['bookrecno']!;
-    var isbn = bookCoverImage.attributes['isbn']!;
-    var callNo = getBookInfo('span', '.callnosSpan');
-    var publishDate =
-        getBookInfo('div', 'div').split('出版日期:')[1].split('\n')[0].trim();
+    var htmlElement = BeautifulSoup(response.data);
 
-    var publisher = getBookInfo('a', '.publisher-link');
-    var title = getBookInfo('a', '.title-link');
-    return Book(bookId, isbn, title, author, publisher, publishDate, callNo);
+    var currentPage =
+        htmlElement.find('b', selector: '.meneame > b')!.text.trim();
+    var resultNumAndTime = htmlElement
+        .find(
+          'div',
+          selector: '#search_meta > div:nth-child(1)',
+        )!
+        .text;
+    var resultCount = int.parse(RegExp(r'检索到: (\S*) 条结果')
+        .allMatches(resultNumAndTime)
+        .first
+        .group(1)!
+        .replaceAll(',', ''));
+    var useTime = double.parse(
+        RegExp(r'检索时间: (\S*) 秒').allMatches(resultNumAndTime).first.group(1)!);
+    var totalPages = htmlElement
+        .find('div', class_: 'meneame')!
+        .find('span', class_: 'disabled')!
+        .text
+        .trim();
+
+    return BookSearchResult(
+        resultCount,
+        useTime,
+        int.parse(currentPage),
+        int.parse(totalPages
+            .substring(1, totalPages.length - 1)
+            .trim()
+            .replaceAll(',', '')),
+        htmlElement
+            .find('table', class_: 'resultTable')!
+            .findAll('tr')
+            .map((e) => parseBook(e))
+            .toList());
   }
-
-  var htmlElement = BeautifulSoup(response.data);
-
-  var currentPage =
-      htmlElement.find('b', selector: '.meneame > b')!.text.trim();
-  var resultNumAndTime = htmlElement
-      .find(
-        'div',
-        selector: '#search_meta > div:nth-child(1)',
-      )!
-      .text;
-  var resultCount = int.parse(RegExp(r'检索到: (\S*) 条结果')
-      .allMatches(resultNumAndTime)
-      .first
-      .group(1)!
-      .replaceAll(',', ''));
-  var useTime = double.parse(
-      RegExp(r'检索时间: (\S*) 秒').allMatches(resultNumAndTime).first.group(1)!);
-  var totalPages = htmlElement
-      .find('div', class_: 'meneame')!
-      .find('span', class_: 'disabled')!
-      .text
-      .trim();
-
-  return BookSearchResult(
-      resultCount,
-      useTime,
-      int.parse(currentPage),
-      int.parse(totalPages
-          .substring(1, totalPages.length - 1)
-          .trim()
-          .replaceAll(',', '')),
-      htmlElement
-          .find('table', class_: 'resultTable')!
-          .findAll('tr')
-          .map((e) => parseBook(e))
-          .toList());
 }
