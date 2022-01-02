0f858bc759d7d32d828e059eebb58743c3ea1082
Zhangzqs <i@zhangzqs.cn>
2022-01-02 23:12:40 +0800

    Update: 完善课表查询, 添加学年学期

--- lib/services/edu/src/timetable.dart
+++ lib/services/edu/src/timetable.dart
@@ -3,6 +3,24 @@ import 'dart:core';
 import 'package:kite/services/edu/src/timetable_parser.dart';
 import 'package:kite/services/sso/src/session.dart';
 
+enum Semester {
+  all,
+  firstTerm,
+  secondTerm,
+  midTerm,
+}
+
+class SchoolYear {
+  static const all = SchoolYear(null);
+  final int? _year;
+  const SchoolYear(this._year);
+
+  @override
+  String toString() {
+    return (_year ?? '').toString();
+  }
+}
+
 class Timetable {
   static const _timeTableUrl =
       'http://jwxt.sit.edu.cn/jwglxt/kbcx/xskbcx_cxXsgrkb.html';
@@ -11,20 +29,31 @@ class Timetable {
 
   const Timetable(this._session);
 
+  static String _semesterToRequestField(Semester semester) {
+    return {
+      Semester.all: '',
+      Semester.firstTerm: '3',
+      Semester.secondTerm: '12',
+      Semester.midTerm: '16',
+    }[semester]!;
+  }
+
   /// 获取课表
-  Future<List<Course>> getTimetable() async {
+  Future<List<Course>> getTimetable(
+    SchoolYear schoolYear,
+    Semester semester,
+  ) async {
     var response = await _session.post(
       _timeTableUrl,
       queryParameters: {
-        'gnmkdm': '',
+        'gnmkdm': 'N253508',
         'su': _session.username!,
       },
       data: {
         // 学年名
-        'xnm': 2021,
+        'xnm': schoolYear.toString(),
         // 学期名
-        'xqm': 3,
-        'kzlx': 'ck',
+        'xqm': _semesterToRequestField(semester),
       },
     );
     return parseTimetable(response.data);
