8e16e7d6255dd690b3df445e7ffbf42fe5085d7d
B635 <frankxck@gmail.com>
2022-01-03 20:49:32 +0800

    Add: 添加成绩爬虫

--- lib/services/edu/src/score.dart
+++ lib/services/edu/src/score.dart
@@ -1,6 +1,42 @@
+import 'package:kite/services/edu/edu.dart';
+import 'package:kite/services/edu/src/score_parser.dart';
 import 'package:kite/services/sso/src/session.dart';
 
 class ScoreService {
-  Session _session;
-  ScoreService(this._session);
+  static const _scoreUrl =
+      'http://jwxt.sit.edu.cn/jwglxt/cjcx/cjcx_cxDgXscj.html';
+
+  final Session _session;
+
+  const ScoreService(this._session);
+
+  static String _semesterToRequestField(Semester semester) {
+    return {
+      Semester.all: '',
+      Semester.firstTerm: '3',
+      Semester.secondTerm: '12',
+      Semester.midTerm: '16',
+    }[semester]!;
+  }
+
+  /// 获取成绩
+  Future<List<Score>> getScoreList(
+    SchoolYear schoolYear,
+    Semester semester,
+  ) async {
+    var response = await _session.post(
+      _scoreUrl,
+      queryParameters: {
+        'qnmkdm': 'N305005',
+        'su': _session.username!,
+      },
+      data: {
+        // 学年名
+        'xnm': schoolYear.toString(),
+        // 学期名
+        'xqm': _semesterToRequestField(semester),
+      },
+    );
+    return parseScoreListPage(response.data);
+  }
 }
