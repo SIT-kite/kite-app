7c0dbeafbd404449093aff3eee07bc88fe2b30cd
B635 <frankxck@gmail.com>
2022-01-03 20:51:05 +0800

    Add: 添加成绩测试

--- test/services/edu/score_test.dart
+++ test/services/edu/score_test.dart
@@ -1,39 +1,19 @@
-import 'dart:convert';
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:kite/services/edu/edu.dart';
 import 'package:kite/services/sso/sso.dart';
 import 'package:logger/logger.dart';
 
 void main() {
   var logger = Logger();
   test('edu test', () async {
-    var username = '';
-    var password = '';
-    var session = Session();
-    await session.login(username, password);
-    // 先跳转到教务首页
+    Session session = Session();
+    await session.login('', '');
     await session.get('http://jwxt.sit.edu.cn/sso/jziotlogin');
-    // 然后成绩查询
-    var scoreQueryUrl = 'http://jwxt.sit.edu.cn/jwglxt/cjcx/cjcx_cxXsgrcj.html';
-    var result = await session.post(
-      scoreQueryUrl,
-      queryParameters: {
-        'doType': 'query',
-        'gnmkdm': 'N305005',
-        'su': username,
-      },
-      data: {
-        'xnm': 2021,
-        'xqm': 3,
-        '_search': false,
-        'nd': 1641027685201,
-        'queryModel.showCount': 15,
-        'queryModel.currentPage': 1,
-        'queryModel.sortName': '',
-        'queryModel.sortOrder': 'asc',
-        'time': 0,
-      },
+    var table = await ScoreService(session).getScoreList(
+      const SchoolYear(2021),
+      Semester.firstTerm,
     );
-    logger.i(jsonDecode(result.toString()));
+    logger.i(table);
   });
 }
