7ebf2ab9cf4480ee503272eb2c5f58f49e5e4957
sunnysab <i@sunnysab.cn>
2022-01-11 12:47:19 +0800

    Add: 添加 Bugly 依赖.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bugly/flutter_bugly.dart';
 
 import 'package:kite/app.dart';
 
 void main() async {
-  runApp(const KiteApp());
+  // 使用说明
+  // https://pub.dev/packages/flutter_bugly
+  FlutterBugly.postCatchedException(() {
+    runApp(const KiteApp());
+    FlutterBugly.init(
+      androidAppId: "a83ed5243d",
+      iOSAppId: "7d8c9907b5",
+    );
+  });
 }
