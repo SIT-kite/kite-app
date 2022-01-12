a3a4d36910ea33673dcfdfa9f0be99bf168712b4
B635 <frankxck@gmail.com>
2022-01-12 22:23:29 +0800

    Add: 添加考场信息爬虫

--- lib/services/edu/edu.dart
+++ lib/services/edu/edu.dart
@@ -1,5 +1,6 @@
 library edu;
 
+export 'src/exam_room.dart';
 export 'src/score.dart';
 export 'src/score_detail.dart';
 export 'src/timetable.dart';
---
+++ lib/services/edu/src/exam_room.dart
@@ -0,0 +1,43 @@
+import 'package:kite/services/edu/edu.dart';
+import 'package:kite/services/edu/src/exam_room_parser.dart';
+import 'package:kite/services/sso/sso.dart';
+
+class ExamRoomService {
+  static const _examRoomUrl =
+      'http://http://jwxt.sit.edu.cn/jwglxt/kwgl/kscx_cxXsksxxIndex.html';
+
+  final ISession _session;
+
+  const ExamRoomService(this._session);
+
+  static String _semesterToRequestField(Semester semester) {
+    return {
+      Semester.all: '',
+      Semester.firstTerm: '3',
+      Semester.secondTerm: '12',
+      Semester.midTerm: '16',
+    }[semester]!;
+  }
+
+  /// 获取考场信息
+  Future<List<ExamRoom>> getExamRoomList(
+    SchoolYear schoolYear,
+    Semester semester,
+  ) async {
+    var response = await _session.post(
+      _examRoomUrl,
+      queryParameters: {
+        'gnmkdm': 'N358105',
+        // 实测以下被注释的字段根本无用，又提高了耦合
+        // 'su': _session.username!,
+      },
+      data: {
+        // 学年名
+        'xnm': schoolYear.toString(),
+        // 学期名
+        'xqm': _semesterToRequestField(semester),
+      },
+    );
+    return parseExamRoomPage(response.data);
+  }
+}
