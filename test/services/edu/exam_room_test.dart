61f44026a4b46fadd4cb43a039110714572dbf54
B635 <frankxck@gmail.com>
2022-01-12 22:25:10 +0800

    Add: 添加考场信息测试

---
+++ test/services/edu/exam_room_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:kite/services/edu/edu.dart';
+import 'package:kite/services/edu/src/edu_session.dart';
+import 'package:kite/services/sso/src/session.dart';
+import 'package:logger/logger.dart';
+
+void main() {
+  var logger = Logger();
+  test('timetable test', () async {
+    Session session = Session();
+    await session.login('', '');
+    var eduSession = EduSession(session);
+    var table = await ExamRoomService(eduSession).getExamRoomList(
+      const SchoolYear(2021),
+      Semester.firstTerm,
+    );
+    logger.i(table);
+  });
+}
