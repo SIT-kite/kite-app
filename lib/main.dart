1deee1232a8f963e9386a42ca35486aba3c9ae55
Zhangzqs <i@zhangzqs.cn>
2022-01-13 00:23:04 +0800

    Fix: 修复了浏览器，桌面端调试出错的问题

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,30 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bugly/flutter_bugly.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
-
 import 'package:kite/app.dart';
 
 void main() async {
   // 使用说明
   // https://pub.dev/packages/flutter_bugly
-  FlutterBugly.postCatchedException(() {
-    runApp(Phoenix(
-      child: const KiteApp(),
-    ));
+  try {
+    if (Platform.isAndroid || Platform.isIOS) {
+      // Android/IOS使用Bugly
+      FlutterBugly.postCatchedException(() {
+        runApp(Phoenix(child: const KiteApp()));
 
-    FlutterBugly.init(
-      androidAppId: "a83ed5243d",
-      iOSAppId: "7d8c9907b5",
-    );
-  });
+        FlutterBugly.init(
+          androidAppId: "a83ed5243d",
+          iOSAppId: "7d8c9907b5",
+        );
+      });
+    } else {
+      // 桌面端不使用Bugly
+      runApp(Phoenix(child: const KiteApp()));
+    }
+  } on Error catch (_, e) {
+    // Web端不支持判定平台，也不使用Bugly
+    runApp(Phoenix(child: const KiteApp()));
+  }
 }
