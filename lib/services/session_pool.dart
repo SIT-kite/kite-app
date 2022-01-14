c724b05d1299077ceb03df390b81e0e9db890b23
sunnysab <i@sunnysab.cn>
2022-01-14 13:08:26 +0800

    Add: SessionPool 添加全局 cookieJar.

--- lib/services/session_pool.dart
+++ lib/services/session_pool.dart
@@ -1,7 +1,9 @@
+import 'package:cookie_jar/cookie_jar.dart';
 import 'package:kite/services/edu/src/edu_session.dart';
 import 'package:kite/services/sso/sso.dart';
 
 class SessionPool {
-  static final Session ssoSession = Session();
+  static final DefaultCookieJar cookieJar = DefaultCookieJar();
+  static final Session ssoSession = Session(jar: cookieJar);
   static final EduSession eduSession = EduSession(ssoSession);
 }
