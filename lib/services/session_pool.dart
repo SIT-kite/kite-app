428a977461ed80ba9e95290c10fe45702306db3d
sunnysab <i@sunnysab.cn>
2022-01-16 11:05:02 +0800

    Update: 各 Session 使用全局 Dio 对象.

--- lib/services/session_pool.dart
+++ lib/services/session_pool.dart
@@ -1,9 +1,21 @@
 import 'package:cookie_jar/cookie_jar.dart';
+import 'package:dio/dio.dart';
 import 'package:kite/services/edu/src/edu_session.dart';
 import 'package:kite/services/sso/sso.dart';
 
 class SessionPool {
-  static final DefaultCookieJar cookieJar = DefaultCookieJar();
-  static final SsoSession ssoSession = SsoSession(jar: cookieJar);
+  static final Dio dio = initDioInstance();
+  static final DefaultCookieJar _cookieJar = DefaultCookieJar();
+
+  static DefaultCookieJar get cookieJar => _cookieJar;
+
+  static final SsoSession ssoSession = SsoSession(dio: dio, jar: _cookieJar);
   static final EduSession eduSession = EduSession(ssoSession);
+
+  static Dio initDioInstance() {
+    Dio dio = Dio();
+    // Add initialization code here.
+
+    return dio;
+  }
 }
