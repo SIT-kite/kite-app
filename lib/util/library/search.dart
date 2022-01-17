4e3a9fc38756bb96213acd363494fb88627a64a4
Zhangzqs <i@zhangzqs.cn>
2022-01-17 17:09:51 +0800

    Update: 提供了图书，图片，馆藏的关联查询的工具

--- lib/util/library/search.dart
+++ lib/util/library/search.dart
@@ -1,18 +1,26 @@
 import 'package:kite/entity/library/book_image.dart';
 import 'package:kite/entity/library/book_search.dart';
+import 'package:kite/entity/library/holding_preview.dart';
 
-class BookWithImage {
+class BookImageHolding {
   Book book;
   BookImage? image;
+  List<HoldingPreviewItem>? holding;
 
-  BookWithImage(this.book, {this.image});
+  BookImageHolding(this.book, {this.image, this.holding});
 
-  /// 使得独立的图书列表与图书图片信息相关联
-  static List<BookWithImage> buildByJoin(
-      List<Book> books, Map<String, BookImage> imageMap) {
-    return books
-        .map((book) =>
-            BookWithImage(book, image: imageMap[book.isbn.replaceAll('-', '')]))
-        .toList();
+  /// 使得图书列表,图书图片信息,馆藏信息相关联
+  static List<BookImageHolding> build(
+    List<Book> books,
+    Map<String, BookImage> imageMap,
+    Map<String, List<HoldingPreviewItem>> holdingMap,
+  ) {
+    return books.map((book) {
+      return BookImageHolding(
+        book,
+        image: imageMap[book.isbn.replaceAll('-', '')],
+        holding: holdingMap[book.bookId],
+      );
+    }).toList();
   }
 }
