5c90a51935fa8db4fa6703a5333bcd2496ac5bac
Zhangzqs <i@zhangzqs.cn>
2022-01-17 23:39:37 +0800

    Update: 为Path工具类添加新的测试用例

--- test/util/path_test.dart
+++ test/util/path_test.dart
@@ -8,5 +8,12 @@ void main() {
     expect(Path(base: 'ABC/', separator: ',').forward('nextName').toString(), 'ABC/,nextName');
     expect(Path().forward('nextName').forward('next2').toString(), '/nextName/next2');
     expect(Path().forward('nextName').forward('next2').backward().toString(), '/nextName');
+
+    var _namespace = Path(base: '/auth');
+    var _usernameKey = _namespace.forward('username').toString();
+    var _passwordKey = _namespace.forward('password').toString();
+    expect(_namespace.toString(), '/auth');
+    expect(_usernameKey, '/auth/username');
+    expect(_passwordKey, '/auth/password');
   });
 }
