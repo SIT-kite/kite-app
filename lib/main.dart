fecd0a14080f06d1dda0d6d57f621ff51a4ad39d
Zhangzqs <i@zhangzqs.cn>
2022-01-17 22:06:14 +0800

    Refactor: 重构所有Pool的初始化结构

--- lib/main.dart
+++ lib/main.dart
@@ -5,20 +5,30 @@ import 'package:flutter_bugly/flutter_bugly.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:kite/app.dart';
 import 'package:kite/storage/storage_pool.dart';
+import 'package:kite/util/logger.dart';
 
 import 'service/session_pool.dart';
+
+/// 应用启动前需要的初始化
+Future<void> init() async {
+  // 初始化顺序交换会引发未知bug
+
+  // 先初始化本地持久化层
+  await StoragePool.init();
+  await SessionPool.init();
+}
 
 void main() async {
   // 使用说明
   // https://pub.dev/packages/flutter_bugly
+  Log.info('当前的系统版本：' + Platform.operatingSystem);
   try {
     if (Platform.isAndroid || Platform.isIOS) {
       // Android/IOS使用 Bugly
       FlutterBugly.postCatchedException(() async {
         runApp(Phoenix(child: const KiteApp()));
         // 初始化持久化池
-        await StoragePool.init();
-        SessionPool.initUserAgentString();
+        await init();
         FlutterBugly.init(
           androidAppId: "a83ed5243d",
           iOSAppId: "7d8c9907b5",
@@ -26,12 +36,14 @@ void main() async {
       });
     } else {
       // 桌面端不使用 Bugly
-      await StoragePool.init();
+      Log.info('当前环境: Desktop');
       runApp(Phoenix(child: const KiteApp()));
+      await init();
     }
-  } on Error catch (_, e) {
+  } on Error catch (_, __) {
     // Web端不支持判定平台，也不使用 Bugly
-    await StoragePool.init();
+    Log.info('当前环境: Web');
     runApp(Phoenix(child: const KiteApp()));
+    await init();
   }
 }
