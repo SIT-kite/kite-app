79782960adcb403a3c65b98b42ff1ded62aea45f
sunnysab <i@sunnysab.cn>
2022-01-17 11:01:46 +0800

    Update: 更新 EduSession 登录判断逻辑, 简单重命名.

--- lib/services/edu/edu_session.dart
+++ lib/services/edu/edu_session.dart
@@ -3,14 +3,15 @@ import 'package:kite/services/abstract_session.dart';
 
 class EduSession extends ASession {
   final ASession _session;
+
   EduSession(this._session);
 
-  Future<void> _getCookie(Response response) async {
+  Future<void> _refreshCookie() async {
     await _session.get('http://jwxt.sit.edu.cn/sso/jziotlogin');
   }
 
-  bool _isEduLoginPage(Response response) {
-    return response.data.runtimeType == String && (response.data as String).contains('用户登录');
+  bool _isRedirectedToLoginPage(Response response) {
+    return response.realUri.path == '/jwglxt/xtgl/login_slogin.html';
   }
 
   @override
@@ -34,13 +35,12 @@ class EduSession extends ASession {
       );
     }
 
-    var response = await fetch();
-    // 如果返回值是登陆页面，那就从sso跳转过来
-    if (!_isEduLoginPage(response)) {
-      return response;
+    Response response = await fetch();
+    // 如果返回值是登录页面，那就从 SSO 跳转一次以登录.
+    if (_isRedirectedToLoginPage(response)) {
+      await _refreshCookie();
+      response = await fetch();
     }
-    await _getCookie(response);
-    // 再一次发请求
-    return await fetch();
+    return response;
   }
 }
