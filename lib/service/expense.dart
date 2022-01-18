6af7b173754146f440a7675ae62d536b942438b3
Zhangzqs <i@zhangzqs.cn>
2022-01-19 02:08:52 +0800

    Add&Fix&&Refactor: 重构并修复了消费爬虫，添加日期请求，页数解析

--- lib/service/expense.dart
+++ lib/service/expense.dart
@@ -1,15 +1,12 @@
 import 'package:beautiful_soup_dart/beautiful_soup.dart';
 import 'package:dio/dio.dart';
+import 'package:enough_convert/gbk/gbk.dart';
+import 'package:kite/dao/expense.dart';
 import 'package:kite/entity/expense.dart';
-import 'package:kite/service/sso/sso_session.dart';
+import 'package:kite/service/abstract_service.dart';
+import 'package:kite/service/abstract_session.dart';
 
-const String noticeSelector = 'table#table > tbody > tr';
-const String dateSelector = 'td:nth-child(5) > div';
-const String timeSelector = 'td:nth-child(7) > div';
-const String amountSelector = 'td:nth-child(9) > div';
-const String placeSelector = 'td:nth-child(11) > div';
-
-class ExpenseService {
+class ExpenseService extends AService implements ExpenseDao {
   static const _expenseUrl = 'http://card.sit.edu.cn/personalxiaofei.jsp';
   static const _typeKeywords = {
     '热水': ExpenseType.water,
@@ -18,51 +15,76 @@ class ExpenseService {
     '食堂': ExpenseType.canteen,
     '超市': ExpenseType.store,
   };
+  static const _codec = GbkCodec();
+  ExpenseService(ASession session) : super(session);
 
-  final SsoSession _session;
-
-  const ExpenseService(this._session);
-
-  Future<List<ExpenseDetail>> getExpenseBill(int page) async {
-    final response = await _session.get(
+  @override
+  Future<ExpensePage> getExpensePage(int page, {DateTime? start, DateTime? end}) async {
+    start = start ?? DateTime(2010);
+    end = end ?? DateTime.now();
+    final response = await session.get(
       _expenseUrl,
       queryParameters: {
         'page': page.toString(),
+        'from': _convert(start),
+        'to': _convert(end),
       },
-      responseType: ResponseType.plain,
+      responseType: ResponseType.bytes,
     );
-    return parseExpenseDetail(response.data);
+    return _parseExpenseDetail(_codec.decode(response.data));
   }
 
-  static List<ExpenseDetail> parseExpenseDetail(String htmlPage) {
+  static ExpensePage _parseExpenseDetail(String htmlPage) {
+    const String recordSelector = 'table#table > tbody > tr';
+
     final BeautifulSoup soup = BeautifulSoup(htmlPage);
+    // 先获取每一行,过滤首行
+    final records = soup.findAll(recordSelector).sublist(1).map(_parseExpenseItem).toList();
 
-    return soup.findAll(noticeSelector).map(parseExpenseItem).toList();
+    // 页号信息
+    final pageInfo = soup.findAll('div', id: 'listContent')[1].text;
+    String currentPage = RegExp(r'第(\S*)页').allMatches(pageInfo).first.group(1)!;
+    String totalPage = RegExp(r'共(\S*)页').allMatches(pageInfo).first.group(1)!;
+
+    return ExpensePage()
+      ..records = records
+      ..currentPage = int.parse(currentPage)
+      ..total = int.parse(totalPage);
   }
 
+  static String _convert(DateTime date) {
+    final year = date.year.toString();
+    var month = date.month.toString();
+    var day = date.day.toString();
+    if (month.length == 1) {
+      month = '0$month';
+    }
+    if (day.length == 1) {
+      day = '0$day';
+    }
+    return '$year$month$day';
+  }
+
+  /// 根据某地点计算其消费类型
   static ExpenseType _speculateType(String place) {
-    ExpenseType result = ExpenseType.unknown;
-    for (String key in _typeKeywords.keys) {
-      if (place.contains(key)) {
-        result = _typeKeywords[key]!;
-        break;
-      }
-    }
-    return result;
+    return _typeKeywords.entries.firstWhere(
+      (element) => place.contains(element.key),
+      orElse: () {
+        return const MapEntry('', ExpenseType.unknown);
+      },
+    ).value;
   }
 
-  static ExpenseDetail parseExpenseItem(Bs4Element item) {
-    if (item.findAll('th').isNotEmpty) {
-      final String place = item.find(placeSelector)!.innerHtml.trim();
-      final ExpenseType type = _speculateType(place);
-      final String amount = item.find(amountSelector)!.innerHtml.trim();
+  /// 解析每一条消费记录
+  static ExpenseRecord _parseExpenseItem(Bs4Element item) {
+    final cells = item.findAll('td').map((e) => e.text.trim()).toList();
 
-      final String date = item.find(dateSelector)!.innerHtml.trim();
-      final String time = item.find(timeSelector)!.innerHtml.trim();
-      final DateTime ts = DateTime.parse('$date $time');
-
-      return ExpenseDetail(place, ts, double.parse(amount), type);
-    }
-    throw Exception('消费记录解析失败.');
+    return ExpenseRecord()
+      ..username = cells[0]
+      ..name = cells[1]
+      ..ts = DateTime.parse('${cells[2]} ${cells[3]}')
+      ..amount = double.parse(cells[4])
+      ..place = cells[5]
+      ..type = _speculateType(cells[5]);
   }
 }
