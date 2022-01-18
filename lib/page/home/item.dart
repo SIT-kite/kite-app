af5c8f5b1166bac4313d9ad2b4c32cfc4cadce7c
sunnysab <i@sunnysab.cn>
2022-01-18 08:11:04 +0800

    Refactor: 简化首页 item 代码.

--- lib/page/home/item.dart
+++ lib/page/home/item.dart
@@ -15,39 +15,30 @@ class _HomeItemState extends State<HomeItem> {
   String routeName;
   AssetImage icon;
   String title;
-  String content = "加载中";
+  String subtitle = "加载中";
 
   _HomeItemState(this.routeName, this.icon, this.title);
 
   @override
   Widget build(BuildContext context) {
     final titleStyle = const TextStyle().copyWith(fontSize: 16, fontWeight: FontWeight.bold);
-    final contentStyle = const TextStyle().copyWith(fontSize: 15);
+    final subtitleStyle = const TextStyle().copyWith(fontSize: 15);
 
-    return InkWell(
-        onTap: () {
-          Navigator.of(context).pushNamed(routeName);
-        },
-        child: Container(
-          padding: const EdgeInsets.fromLTRB(8, 8, 8, 8),
-          decoration: BoxDecoration(
-            color: Colors.white.withOpacity(0.6),
-          ),
-          child: Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            SizedBox(
-              height: 40,
-              width: 40,
-              child: Center(
-                child: Image(image: icon, width: 30, height: 30),
-              ),
-            ),
-            const SizedBox(width: 8),
-            Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Row(children: [Text(title, style: titleStyle)]),
-              const SizedBox(height: 2.0),
-              Row(children: [Text(content, style: contentStyle)]),
-            ])
-          ]),
-        ));
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).pushNamed(routeName);
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white.withOpacity(0.6),
+        ),
+        child: ListTile(
+          leading: Image(image: icon, height: 30, width: 30),
+          title: Text(title, style: titleStyle),
+          subtitle: Text(subtitle, style: subtitleStyle),
+          dense: true,
+        ),
+      ),
+    );
   }
 }
