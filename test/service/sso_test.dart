dfcc2e135da8eb93ff50a5664aad800c1a15fdb9
Zhangzqs <i@zhangzqs.cn>
2022-01-19 02:09:44 +0800

    Fix: 修复sso测试

--- test/service/sso_test.dart
+++ test/service/sso_test.dart
@@ -1,10 +1,13 @@
 import 'package:beautiful_soup_dart/beautiful_soup.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:kite/service/sso.dart';
+import 'package:kite/global/session_pool.dart';
+import 'package:kite/global/storage_pool.dart';
 
-void main() {
+void main() async {
+  await StoragePool.init();
+  await SessionPool.init();
   test('test login', () async {
-    var session = SsoSession();
+    var session = SessionPool.ssoSession;
     var a = await session.login('', '');
     var index = await session.get('https://myportal.sit.edu.cn/');
     var list = BeautifulSoup(index.data)
