ea0c7cd43e206e1555334dcbe5238dc71b3da8e9
sunnysab <i@sunnysab.cn>
2022-01-19 20:34:04 +0800

    Update: 更新 OCR 接口地址, 优化代码.

--- lib/service/ocr.dart
+++ lib/service/ocr.dart
@@ -3,22 +3,15 @@ import 'package:dio/dio.dart';
 class OcrRecognizeException implements Exception {
   final int code;
   final String msg;
-  const OcrRecognizeException(this.code, this.msg);
 
-  @override
-  String toString() {
-    return "{code: $code, msg: $msg}";
-  }
+  const OcrRecognizeException(this.code, this.msg);
 }
 
 class OcrServer {
-  static const _ocrServerUrl = 'http://10.1.160.21:5000';
+  static const _ocrServerUrl = 'https://kite.sunnysab.cn/api/ocr/captcha';
 
   static recognize(String imageBase64) async {
-    var response = await Dio().post(
-      '$_ocrServerUrl/ocr/captcha',
-      data: imageBase64,
-    );
+    var response = await Dio().post(_ocrServerUrl, data: imageBase64);
     var result = response.data;
     var code = result['code'];
     if (result['code'] == 0) {
