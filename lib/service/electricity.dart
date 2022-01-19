cb7c887db7099649f47fc98c63fb0c027ecc66ea
Pony-Zhang <sherly_cs_sh@163.com>
2022-01-19 21:37:55 +0800

    Add: 添加获取电费余额接口

---
+++ lib/service/electricity.dart
@@ -0,0 +1,47 @@
+import 'package:dio/dio.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'electricity.g.dart';
+
+enum _Mode {
+  balance,
+  rank,
+  condition_hours,
+  condition_days,
+}
+
+String _getElectricityUrl(String room, _Mode mode) {
+  switch (mode) {
+    case _Mode.balance:
+      return 'https://kite.sunnysab.cn/api/v2/electricity/room/$room';
+    case _Mode.rank:
+      return 'https://kite.sunnysab.cn/api/v2/electricity/room/$room/rank';
+    case _Mode.condition_hours:
+      return 'https://kite.sunnysab.cn/api/v2/electricity/room/$room/bill/hours';
+    case _Mode.condition_days:
+      return 'https://kite.sunnysab.cn/api/v2/electricity/room/$room/bill/days';
+  }
+}
+
+@JsonSerializable()
+class Balance {
+  double balance;
+  double power;
+  String ts;
+
+  Balance(this.balance, this.power, this.ts);
+
+  factory Balance.fromJson(Map<String, dynamic> json) =>
+      _$BalanceFromJson(json);
+}
+
+
+
+
+Future<Balance> getBalance(String room) async {
+  final url = _getElectricityUrl(room, _Mode.balance);
+  final response = await Dio().get(url);
+  final balance = Balance.fromJson(response.data['data']);
+
+  return balance;
+}
