1317eb9ba93ca9c4df070e8c2a9af47f64afea70
Zhangzqs <i@zhangzqs.cn>
2022-01-19 13:47:09 +0800

    Add: 添加可以很简单地查询一批书的图片与馆藏信息的工具函数

--- lib/util/library/search.dart
+++ lib/util/library/search.dart
@@ -1,3 +1,5 @@
+import 'package:kite/dao/library/holding_preview.dart';
+import 'package:kite/dao/library/image_search.dart';
 import 'package:kite/entity/library/book_image.dart';
 import 'package:kite/entity/library/book_search.dart';
 import 'package:kite/entity/library/holding_preview.dart';
@@ -22,5 +24,24 @@ class BookImageHolding {
         holding: holdingMap[book.bookId],
       );
     }).toList();
+  }
+
+  /// 可以很简单地查询一批书的图片与馆藏信息
+  static Future<List<BookImageHolding>> simpleQuery(
+    BookImageSearchDao bookImageSearchDao,
+    HoldingPreviewDao holdingPreviewDao,
+    List<Book> books,
+  ) async {
+    final imageHoldingPreview = await Future.wait([
+      bookImageSearchDao.searchByBookList(books),
+      holdingPreviewDao.getHoldingPreviews(books.map((e) => e.bookId).toList()),
+    ]);
+    final imageResult = imageHoldingPreview[0] as Map<String, BookImage>;
+    final holdingPreviewResult = imageHoldingPreview[1] as HoldingPreviews;
+    return BookImageHolding.build(
+      books,
+      imageResult,
+      holdingPreviewResult.previews,
+    );
   }
 }
