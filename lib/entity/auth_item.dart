a50a5caa7c43142f75a06ff673fb8f2eeac3e923
Zhangzqs <i@zhangzqs.cn>
2022-01-19 11:30:32 +0800

    Refactor: 抽离hive的typeId定义

--- lib/entity/auth_item.dart
+++ lib/entity/auth_item.dart
@@ -1,9 +1,10 @@
 import 'package:hive/hive.dart';
+import 'package:kite/global/hive_type_id_pool.dart';
 
 part 'auth_item.g.dart';
 
 /// 用于存放认证信息
-@HiveType(typeId: 2)
+@HiveType(typeId: HiveTypeIdPool.authItemTypeId)
 class AuthItem extends HiveObject {
   @HiveField(0, defaultValue: '')
   String username = '';
---
+++ lib/global/hive_type_id_pool.dart
@@ -0,0 +1,5 @@
+/// 存放所有 hive 的自定义类型的typeId
+class HiveTypeIdPool {
+  static const librarySearchHistoryItem = 1;
+  static const authItemTypeId = 2;
+}
