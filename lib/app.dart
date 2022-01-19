208a22a5a1299b7af4bc23c352336fddfa3cd9af
sunnysab <i@sunnysab.cn>
2022-01-19 23:24:30 +0800

    Fix: 修复主题颜色设置无法应用的问题.

--- lib/app.dart
+++ lib/app.dart
@@ -27,7 +27,9 @@ class KiteApp extends StatelessWidget {
       '/setting': (context) => const SettingPage(),
     };
 
-    final themeData = ThemeData.light().copyWith(primaryColor: StoragePool.themeSetting.color);
+    // primarySwatch should be set, by https://github.com/flutter/flutter/issues/82996.
+    final primaryColor = StoragePool.themeSetting.color;
+    final themeData = ThemeData(primaryColor: primaryColor, primarySwatch: createThemeSwatch(primaryColor));
 
     return MaterialApp(
       title: '上应小风筝',
@@ -37,4 +39,24 @@ class KiteApp extends StatelessWidget {
       routes: routes,
     );
   }
+}
+
+MaterialColor createThemeSwatch(Color color) {
+  List strengths = <double>[.05];
+  Map<int, Color> swatch = {};
+  final int r = color.red, g = color.green, b = color.blue;
+
+  for (int i = 1; i < 10; i++) {
+    strengths.add(0.1 * i);
+  }
+  strengths.forEach((strength) {
+    final double ds = 0.5 - strength;
+    swatch[(strength * 1000).round()] = Color.fromRGBO(
+      r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+      g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+      b + ((ds < 0 ? b : (255 - b)) * ds).round(),
+      1,
+    );
+  });
+  return MaterialColor(color.value, swatch);
 }
