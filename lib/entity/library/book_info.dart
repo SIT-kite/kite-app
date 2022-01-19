7bdd1c6e5f7088a617dcc4bc49b2dcefd1821544
Zhangzqs <i@zhangzqs.cn>
2022-01-19 13:02:14 +0800

    Add: 重构图书信息实体风格

--- lib/entity/library/book_info.dart
+++ lib/entity/library/book_info.dart
@@ -1,15 +1,8 @@
 import 'dart:collection';
 
 class BookInfo {
-  final String title;
-  final String isbn;
-  final String price;
-  final LinkedHashMap<String, String> rawDetail;
-
-  const BookInfo(this.title, this.isbn, this.price, this.rawDetail);
-
-  @override
-  String toString() {
-    return 'BookInfo{title: $title, isbn: $isbn, price: $price, rawDetail: $rawDetail}';
-  }
+  String title = '';
+  String isbn = '';
+  String price = '';
+  LinkedHashMap<String, String> rawDetail = LinkedHashMap<String, String>();
 }
