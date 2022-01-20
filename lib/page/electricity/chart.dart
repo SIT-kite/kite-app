79d643e50e3c10cc0383541d41ba4dcf6adfebc2
Pony-Zhang <sherly_cs_sh@163.com>
2022-01-21 00:31:25 +0800

    Fix: 修复电费 Chart 组件参数类型不匹配

--- lib/page/electricity/chart.dart
+++ lib/page/electricity/chart.dart
@@ -1,5 +1,6 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:kite/entity/electricity.dart';
 
 const List<Color> _gradientColors = [
   Color(0xff23b6e6),
@@ -7,8 +8,8 @@ const List<Color> _gradientColors = [
 ];
 
 class Chart extends StatefulWidget {
-  final hoursList;
-  final daysList;
+  final List<ConditionHours> hoursList;
+  final List<ConditionDays> daysList;
 
   const Chart(this.hoursList, this.daysList, {Key? key}) : super(key: key);
 
@@ -17,8 +18,8 @@ class Chart extends StatefulWidget {
 }
 
 class _ChartState extends State<Chart> {
-  final hoursList;
-  final daysList;
+  final List<ConditionHours> hoursList;
+  final List<ConditionDays> daysList;
   bool isShowDays = false;
 
   @override
@@ -27,49 +28,58 @@ class _ChartState extends State<Chart> {
   List<String> _getBottomTitles() {
     List<String> bottomTitles;
     if (isShowDays)
-      bottomTitles = daysList.map((item) =>
-      item.date
-      );
+      bottomTitles = daysList.map((item) => item.date).toList();
     else
-      bottomTitles = hoursList.map((item) =>
-      item.time);
+      bottomTitles = hoursList.map((item) => item.time).toList();
     return bottomTitles;
   }
 
   List<FlSpot> _getAxisYData() {
     List<FlSpot> axisYData;
-    int count = -1;
-    axisYData = (isShowDays ? daysList : hoursList).map((item) {
-      count++;
-      return (FlSpot(
-          count.toDouble(), item.consumption));
+
+    if (isShowDays) {
+      int count = -1;
+      axisYData = daysList.map((item) {
+        count++;
+        return FlSpot(count.toDouble(), item.consumption);
+      }).toList();
+    } else {
+      int count = -1;
+      axisYData = hoursList.map((item) {
+        count++;
+        return FlSpot(count.toDouble(), item.consumption);
+      }).toList();
     }
-    );
     return axisYData;
   }
 
   double _getMaxY() {
     double maxY = 0;
-    (isShowDays? daysList : hoursList).forEach((item) {
-      maxY = maxY > item.consumption ? maxY : item.consumption;
-    });
+    if (isShowDays) {
+      daysList.forEach((item) {
+        maxY = maxY > item.consumption ? maxY : item.consumption;
+      });
+    } else {
+      hoursList.forEach((item) {
+        maxY = maxY > item.consumption ? maxY : item.consumption;
+      });
+    }
     return maxY;
   }
 
   LineChart _buildLineChart(bottomTitles) {
     return LineChart(LineChartData(
-      lineTouchData:
-      LineTouchData(touchTooltipData: LineTouchTooltipData(
+      lineTouchData: LineTouchData(touchTooltipData: LineTouchTooltipData(
           getTooltipItems: (List<LineBarSpot> touchedSpots) {
-            return touchedSpots.map((LineBarSpot touchedSpot) {
-              final textStyle = TextStyle(
-                color: touchedSpot.bar.colors[0],
-                fontWeight: FontWeight.bold,
-                fontSize: 14,
-              );
-              return LineTooltipItem(touchedSpot.y.toStringAsFixed(3), textStyle);
-            }).toList();
-          })),
+        return touchedSpots.map((LineBarSpot touchedSpot) {
+          final textStyle = TextStyle(
+            color: touchedSpot.bar.colors[0],
+            fontWeight: FontWeight.bold,
+            fontSize: 14,
+          );
+          return LineTooltipItem(touchedSpot.y.toStringAsFixed(3), textStyle);
+        }).toList();
+      })),
       gridData: FlGridData(
         show: true,
         drawVerticalLine: true,
@@ -93,13 +103,12 @@ class _ChartState extends State<Chart> {
         bottomTitles: SideTitles(
           showTitles: true,
           reservedSize: 20,
-          interval: isShowDays? 1 : 3,
-          getTextStyles: (context, value) =>
-          const TextStyle(color: Color(0xff68737d),
+          interval: isShowDays ? 1 : 3,
+          getTextStyles: (context, value) => const TextStyle(
+              color: Color(0xff68737d),
               fontWeight: FontWeight.bold,
               fontSize: 14),
-          getTitles: (value) =>
-          bottomTitles.length == 8 && value.toInt() > 7
+          getTitles: (value) => bottomTitles.length == 8 && value.toInt() > 7
               ? ''
               : bottomTitles[value.toInt()],
           margin: 4,
@@ -107,8 +116,7 @@ class _ChartState extends State<Chart> {
         leftTitles: SideTitles(
           showTitles: true,
           interval: 1,
-          getTextStyles: (context, value) =>
-          const TextStyle(
+          getTextStyles: (context, value) => const TextStyle(
             color: Color(0xff67727d),
             fontWeight: FontWeight.bold,
             fontSize: 15,
@@ -118,10 +126,11 @@ class _ChartState extends State<Chart> {
           margin: 8,
         ),
       ),
-      borderData: FlBorderData(show: true,
+      borderData: FlBorderData(
+          show: true,
           border: Border.all(color: const Color(0xff37434d), width: 1)),
       minX: 0,
-      maxX: isShowDays? 7 : 24,
+      maxX: isShowDays ? 7 : 24,
       minY: 0,
       maxY: _getMaxY(),
       lineBarsData: [
@@ -136,8 +145,8 @@ class _ChartState extends State<Chart> {
           ),
           belowBarData: BarAreaData(
             show: true,
-            colors: _gradientColors.map((color) => color.withOpacity(0.3))
-                .toList(),
+            colors:
+                _gradientColors.map((color) => color.withOpacity(0.3)).toList(),
           ),
         ),
       ],
@@ -165,14 +174,14 @@ class _ChartState extends State<Chart> {
           // color: Colors.white,
           child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
             TextButton(
-              onPressed: () =>
-                  setState(() {
-                    isShowDays = !isShowDays;
-                  }),
+              onPressed: () => setState(() {
+                isShowDays = !isShowDays;
+              }),
               child: Row(children: [
                 Text(
                   '过去一天',
-                  style: TextStyle(fontSize: 20,
+                  style: TextStyle(
+                      fontSize: 20,
                       color: isShowDays ? Colors.grey : Colors.blue),
                 ),
                 const Text(
@@ -181,7 +190,8 @@ class _ChartState extends State<Chart> {
                 ),
                 Text(
                   '过去一周',
-                  style: TextStyle(fontSize: 20,
+                  style: TextStyle(
+                      fontSize: 20,
                       color: isShowDays ? Colors.blue : Colors.grey),
                 )
               ]),
@@ -192,8 +202,3 @@ class _ChartState extends State<Chart> {
     );
   }
 }
-
-
-
-
-
