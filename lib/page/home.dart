ce94df2bc4959ea396b832942d12b749b94c148d
sunnysab <i@sunnysab.cn>
2022-01-20 22:53:04 +0800

    Add: 首页添加下拉刷新与登录逻辑.

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:kite/global/bus.dart';
+import 'package:kite/global/session_pool.dart';
 import 'package:kite/global/storage_pool.dart';
+import 'package:kite/service/sso.dart';
 import 'package:kite/service/weather.dart';
+import 'package:kite/util/flash.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:universal_platform/universal_platform.dart';
 
@@ -21,15 +24,53 @@ class HomePage extends StatelessWidget {
 
   void _updateWeather() {
     Future.delayed(const Duration(milliseconds: 800), () async {
-      final weather = await getCurrentWeather(StoragePool.homeSetting.campus);
-      eventBus.emit('onWeatherUpdate', weather);
+      try {
+        final weather = await getCurrentWeather(StoragePool.homeSetting.campus);
+        eventBus.emit('onWeatherUpdate', weather);
+      } catch (_) {}
     });
   }
 
-  void _onHomeRefresh() {
-    _refreshController.refreshCompleted(resetFooterState: true);
+  Future<String?> _doLogin(BuildContext context) async {
+    final String username = StoragePool.authSetting.currentUsername!;
+    final String password = StoragePool.authPool.get(username)!.password;
 
-    eventBus.emit('onHomeRefresh');
+    await SessionPool.ssoSession.login(username, password);
+  }
+
+  Future<void> _onHomeRefresh(BuildContext context) async {
+    // 如果未登录 (老用户直接进入 Home 页不会处于登录状态, 但新用户经过 login 页时已登录)
+    if (!SessionPool.ssoSession.isOnline) {
+      try {
+        await _doLogin(context);
+        showBasicFlash(context, const Text('登录成功'));
+      } on Exception catch (e) {
+        print(e.toString());
+
+        // 如果是认证相关问题, 弹出相应的错误信息.
+        if (e is UnknownAuthException || e is CredentialsInvalidException) {
+          showBasicFlash(context, Text('登录异常: $e'));
+        } else {
+          // 如果是网络问题, 提示检查网络.
+          showBasicFlash(
+            context,
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              Text('${e.runtimeType}, 请确认网络连接'),
+              TextButton(
+                child: const Text('检查网络'),
+                onPressed: () => Navigator.of(context).pushNamed('/connectivity'),
+              )
+            ]),
+            duration: const Duration(seconds: 3),
+          );
+        }
+      }
+    }
+
+    if (SessionPool.ssoSession.isOnline) {
+      eventBus.emit('onHomeRefresh');
+    }
+    _refreshController.refreshCompleted(resetFooterState: true);
   }
 
   Widget _buildTitleLine(BuildContext context) {
@@ -91,7 +132,7 @@ class HomePage extends StatelessWidget {
               ),
             ),
           ]),
-          onRefresh: _onHomeRefresh,
+          onRefresh: () => _onHomeRefresh(context),
         ),
       ],
     );
