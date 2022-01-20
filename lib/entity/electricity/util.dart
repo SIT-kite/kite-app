60c4d3e1e87a2c125086e58461de1ec9902a2e96
Pony-Zhang <sherly_cs_sh@163.com>
2022-01-20 21:47:19 +0800

    Refactor: 重构电费 getRank, 删除冗余代码

--- lib/entity/electricity/util.dart
+++ lib/entity/electricity/util.dart
@@ -9,10 +9,8 @@ String getCharge(List<ConditionDays> list) {
   return charge.toStringAsFixed(2);
 }
 
-Future<Map<String, dynamic>> getRank(String room) {
-  return fetchRank(room).then((res) {
-    final consumption = res.consumption.toStringAsFixed(2);
-    final percentage = (res.rank / res.roomCount * 100).toStringAsFixed(2);
-    return {'consumption': consumption, 'percentage': percentage};
-  });
-}
+Future<Map<String, dynamic>> getRank(String room) =>
+    fetchRank(room).then((res) {
+      final percentage = (res.rank / res.roomCount * 100).toStringAsFixed(2);
+      return {'consumption': res.consumption, 'percentage': percentage};
+    });
