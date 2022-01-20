06763b422493032d8d309b3b4382652751e7ba79
Zhangzqs <i@zhangzqs.cn>
2022-01-20 18:04:22 +0800

    Add: 添加单元测试中常用的函数与配置

---
+++ test/service/mock.dart
@@ -0,0 +1,29 @@
+import 'package:kite/global/session_pool.dart';
+import 'package:kite/global/storage_pool.dart';
+
+// 导出一些测试环境下常用的工具Log
+export 'package:kite/util/logger.dart';
+
+// 这里填写用于测试的用户名密码
+const String username = '';
+const String password = '';
+
+// 如果需要代理，请在这里设置
+const String proxy = '';
+
+/// 测试前调用该函数做初始化
+Future<void> init() async {
+  await SessionPool.init();
+  await StoragePool.init();
+  if (proxy.isNotEmpty) {
+    /// 使用代理
+    StoragePool.network
+      ..useProxy = true
+      ..proxy = proxy;
+  }
+}
+
+/// 如果需要登录，调用该函数
+Future<void> login() async {
+  await SessionPool.ssoSession.login(username, password);
+}
