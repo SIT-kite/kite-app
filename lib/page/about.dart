d2c8c84f9e06f4d0a430dfcf68352cb9211875bf
sunnysab <i@sunnysab.cn>
2022-01-21 11:58:17 +0800

    Update: 关于页使用网页版.

--- lib/page/about.dart
+++ lib/page/about.dart
@@ -1,25 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-const String markdownContent = """
-# 关于我们
-
-这是一个由上海应用技术大学易班工作站主导开发、服务上海应用技术大学学生的应用。
-我们致力于将其打造为校内最现代、最实用的 App。欢迎同学们提出好的点子、参与开发。
-
-## 联系我们
-
-- 在本项目中提交 issue
-- 在 QQ 群中联系管理员反馈。 小程序反馈群 943110696, 2021级易班新生群 147239936（限本校学生加入）
-- 地址: 奉贤校区大学生活动中心309室
-
-## 参与贡献
-
-在每年招新期间，你可以关注一下“上海应用技术大学易班”公众号，或有关QQ群了解招新信息加入校易班工作站（欢迎来技术部！）。
-
-你也可以直接联系我们，联系方式见 “上方” 或对有关项目提交 issue、 pull request，留下你的痕迹。
-
-""";
+const String _aboutUrl = 'https://kite.sunnysab.cn/about';
 
 class AboutPage extends StatelessWidget {
   const AboutPage({Key? key}) : super(key: key);
@@ -30,12 +12,7 @@ class AboutPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('关于'),
       ),
-      body: const SafeArea(
-        child: Markdown(
-          selectable: true,
-          data: markdownContent,
-        ),
-      ),
+      body: const WebView(initialUrl: _aboutUrl),
     );
   }
 }
