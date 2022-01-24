f3e6dff265c32cce9681397311749b01329da79a
Zhangzqs <i@zhangzqs.cn>
2022-01-24 21:58:46 +0800

    Refactor: 再次回到session无泛型时代 Add: 添加KiteSession鉴权拦截器封装 突然发现约束太大，不会写了

--- lib/service/kite/session.dart
+++ lib/service/kite/session.dart
@@ -19,6 +19,7 @@ class KiteSession extends ASession {
     String? contentType,
     ResponseType? responseType,
   }) async {
+    String? token = jwtDao.jwtToken;
     final response = await dio.request(
       url,
       data: data,
@@ -27,16 +28,60 @@ class KiteSession extends ASession {
         method: method,
         contentType: contentType,
         responseType: responseType,
+        headers: token == null ? null : {'Authorization': 'Bearer ' + token},
       ),
     );
-    return response;
+    try {
+      final Map<String, dynamic> responseData = response.data;
+      final responseDataCode = responseData['code'];
+      // 请求正常
+      if (responseDataCode == 0) {
+        // 直接取数据然后返回
+        response.data = responseData['data'];
+        return response;
+      }
+      // 请求异常
+
+      // 存在code,但是不为0
+      if (responseDataCode != null) {
+        throw KiteApiError(responseDataCode, responseData['msg']);
+      }
+    } catch (_) {
+      // api请求格式有误
+      throw KiteApiFormatError(response.data);
+    }
+    // api请求格式有误
+    throw KiteApiFormatError(response.data);
   }
 
   Future<bool> login(String username, String password) async {
+    // TODO
     return true;
   }
 
   Future<bool> createUser(String username, String password) async {
+    // TODO
     return true;
+  }
+}
+
+class KiteApiError implements Exception {
+  final int code;
+  final String? msg;
+  const KiteApiError(this.code, this.msg);
+
+  @override
+  String toString() {
+    return 'KiteApiError{code: $code, msg: $msg}';
+  }
+}
+
+class KiteApiFormatError implements Exception {
+  final dynamic responseData;
+  const KiteApiFormatError(this.responseData);
+
+  @override
+  String toString() {
+    return 'KiteApiFormatError{responseData: $responseData}';
   }
 }
