dce6e2d4d661afd464c26f939aecb25ce918d9c6
Zhangzqs <i@zhangzqs.cn>
2022-01-24 13:42:37 +0800

    Refactor: 重构EventBus的代码风格

--- lib/global/bus.dart
+++ lib/global/bus.dart
@@ -12,37 +12,35 @@ class EventBus {
   factory EventBus() => _singleton;
 
   // 保存事件订阅者队列，key:事件名(id)，value: 对应事件的订阅者队列
-  final _emap = <Object, List<EventCallback>?>{};
+  final _eventMap = <String, List<EventCallback>?>{};
 
   // 添加订阅者
-  void on(eventName, EventCallback f) {
-    _emap[eventName] ??= <EventCallback>[];
-    _emap[eventName]!.add(f);
+  void on(String eventName, EventCallback f) {
+    _eventMap[eventName] ??= <EventCallback>[];
+    _eventMap[eventName]!.add(f);
   }
 
   // 移除订阅者
-  void off(eventName, [EventCallback? f]) {
-    var list = _emap[eventName];
-    if (eventName == null || list == null) {
+  void off(String eventName, [EventCallback? f]) {
+    final list = _eventMap[eventName];
+    if (list == null) {
       return;
     }
     if (f == null) {
-      _emap[eventName] = null;
+      _eventMap[eventName] = null;
     } else {
       list.remove(f);
     }
   }
 
   // 触发事件，事件触发后该事件所有订阅者会被调用
-  void emit(eventName, [arg]) {
-    var list = _emap[eventName];
+  void emit<T>(String eventName, [T? arg]) {
+    final list = _eventMap[eventName];
     if (list == null) {
       return;
     }
-    int len = list.length - 1;
-    // 反向遍历，防止订阅者在回调中移除自身带来的下标错位
-    for (var i = len; i > -1; --i) {
-      list[i](arg);
+    for (final element in list.reversed) {
+      element(arg);
     }
   }
 }
