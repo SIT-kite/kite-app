c945a5d2e5f0fdd1855031da970a002137134aa6
Zhangzqs <i@zhangzqs.cn>
2022-01-24 13:09:40 +0800

    Add: 新增TimetableStorageDao接口

--- lib/dao/edu/timetable.dart
+++ lib/dao/edu/timetable.dart
@@ -3,3 +3,9 @@ import 'package:kite/entity/edu.dart';
 abstract class TimetableDao {
   Future<List<Course>> getTimetable(SchoolYear schoolYear, Semester semester);
 }
+
+abstract class TimetableStorageDao extends TimetableDao {
+  void add(Course item);
+  void delete(String record);
+  void deleteAll();
+}
--- lib/storage/timetable.dart
+++ lib/storage/timetable.dart
@@ -2,20 +2,23 @@ import 'package:hive/hive.dart';
 import 'package:kite/dao/edu/timetable.dart';
 import 'package:kite/entity/edu.dart';
 
-class CourseStorage implements TimetableDao {
+class CourseStorage implements TimetableStorageDao {
   final Box<Course> box;
 
   const CourseStorage(this.box);
 
+  @override
   void add(Course item) {
     box.put(item.courseName.hashCode, item);
   }
 
   /// 根据搜索文字删除
+  @override
   void delete(String record) {
     box.delete(record.hashCode);
   }
 
+  @override
   void deleteAll() {
     box.deleteAll(box.keys.map((e) => e.hashCode));
   }
