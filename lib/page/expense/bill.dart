09cd908a63af0f61591f6e0d033106dfba9639cb
sunnysab <i@sunnysab.cn>
2022-01-24 23:09:19 +0800

    Delete: 移除 "消费" 模块下拉刷新, 解决卡顿问题.

--- lib/page/expense/bill.dart
+++ lib/page/expense/bill.dart
@@ -1,28 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:grouped_list/grouped_list.dart';
 import 'package:kite/entity/expense.dart';
-import 'package:kite/mock/expense.dart';
-import 'package:kite/service/expense.dart';
 import 'package:kite/global/session_pool.dart';
-import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:kite/global/storage_pool.dart';
+import 'package:kite/service/expense.dart';
+
 import 'icon.dart';
 
 class BillPage extends StatelessWidget {
   BillPage({Key? key}) : super(key: key);
-  List<ExpenseRecord> expenseData =
-      StoragePool.expenseRecordStorage.getAllByTimeDesc().length == 0
-          ? []
-          : StoragePool.expenseRecordStorage.getAllByTimeDesc();
-  Future<void> _onBillRefresh(BuildContext context) async {
-    await ExpenseRemoteService(SessionPool.ssoSession).getExpensePage(true, 1);
-    expenseData = StoragePool.expenseRecordStorage.getAllByTimeDesc();
-    _refreshController.refreshCompleted(resetFooterState: true);
-  }
+  List<ExpenseRecord> expenseData = StoragePool.expenseRecordStorage.getAllByTimeDesc();
 
   Future<List<ExpenseRecord>> _expenseBills() async {
-    ExpensePage firstPage = await ExpenseRemoteService(SessionPool.ssoSession)
-        .getExpensePage(false, 1);
+    ExpensePage firstPage = await ExpenseRemoteService(SessionPool.ssoSession).getExpensePage(false, 1);
     List<Future<ExpensePage>> getPage = [];
     for (int i = 2; i < firstPage.total; i++) {
       ExpenseRemoteService(SessionPool.ssoSession).getExpensePage(false, i);
@@ -31,57 +21,47 @@ class BillPage extends StatelessWidget {
     return StoragePool.expenseRecordStorage.getAllByTimeDesc();
   }
 
-  final RefreshController _refreshController =
-      RefreshController(initialRefresh: false);
   Widget _buildBody(BuildContext context, List<ExpenseRecord> expenseData) {
-    return SmartRefresher(
-      enablePullDown: true,
-      enablePullUp: false,
-      controller: _refreshController,
-      onRefresh: () => _onBillRefresh(context),
-      child: GroupedListView<ExpenseRecord, int>(
-        elements: expenseData,
-        groupBy: (element) => element.ts.year * 12 + element.ts.month,
-        useStickyGroupSeparators: true,
-        order: GroupedListOrder.DESC,
-        itemComparator: (item1, item2) => item1.ts.compareTo(item2.ts),
-        // 生成账单项
-        itemBuilder: (context, detail) {
-          final icon = buildIcon(detail.type);
+    return GroupedListView<ExpenseRecord, int>(
+      elements: expenseData,
+      groupBy: (element) => element.ts.year * 12 + element.ts.month,
+      useStickyGroupSeparators: true,
+      order: GroupedListOrder.DESC,
+      itemComparator: (item1, item2) => item1.ts.compareTo(item2.ts),
+      // 生成账单项
+      itemBuilder: (context, detail) {
+        final icon = buildIcon(detail.type);
 
-          return ListTile(
-            leading: icon,
-            title: Text(detail.place),
-            trailing: Text(detail.amount.toString(), textScaleFactor: 1.5),
-            subtitle: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[Text(detail.ts.toString())], // TODO: 可能太长
-            ),
-          );
-        },
-        // 生成每一组的头部
-        groupHeaderBuilder: (ExpenseRecord firstGroupRecord) {
-          double total = 0;
-          int month = firstGroupRecord.ts.month;
-          int year = firstGroupRecord.ts.year;
+        return ListTile(
+          leading: icon,
+          title: Text(detail.place),
+          trailing: Text(detail.amount.toString(), textScaleFactor: 1.5),
+          subtitle: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[Text(detail.ts.toString())], // TODO: 可能太长
+          ),
+        );
+      },
+      // 生成每一组的头部
+      groupHeaderBuilder: (ExpenseRecord firstGroupRecord) {
+        double total = 0;
+        int month = firstGroupRecord.ts.month;
+        int year = firstGroupRecord.ts.year;
 
-          for (var element in expenseData) {
-            total += (element.ts.month == month && element.ts.year == year)
-                ? element.amount
-                : 0;
-          }
-          return ListTile(
-            title: Text('${year} 年${month} 月 ', textScaleFactor: 1.3),
-            subtitle: Text('支出: ${total.toStringAsFixed(2)} 元'),
-          );
-        },
-      ),
+        for (var element in expenseData) {
+          total += (element.ts.month == month && element.ts.year == year) ? element.amount : 0;
+        }
+        return ListTile(
+          title: Text('$year 年$month 月 ', textScaleFactor: 1.3),
+          subtitle: Text('支出: ${total.toStringAsFixed(2)} 元'),
+        );
+      },
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return StoragePool.expenseRecordStorage.getAllByTimeDesc().length == 0
+    return StoragePool.expenseRecordStorage.getAllByTimeDesc().isEmpty
         ? FutureBuilder<List<ExpenseRecord>>(
             future: _expenseBills(),
             builder: (context, snapshot) {
