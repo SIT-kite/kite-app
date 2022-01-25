3ad65b5ca81941c7e9121e1e07145c4c7913f822
Zhangzqs <i@zhangzqs.cn>
2022-01-25 11:30:50 +0800

    Add: 补充eventBus注释

--- lib/util/event_bus.dart
+++ lib/util/event_bus.dart
@@ -1,7 +1,8 @@
 /// 订阅者回调签名
-typedef EventCallback<T> = void Function(T arg);
+typedef EventCallback<T> = void Function(T? arg);
 
 /// 事件总线工具类
+/// E为事件类型，一般使用枚举类型，也可使用其他类型
 class EventBus<E> {
   // 保存事件订阅者队列，key:事件名(id)，value: 对应事件的订阅者队列
   final _eventMap = <E, List<EventCallback>?>{};
@@ -25,7 +26,8 @@ class EventBus<E> {
     }
   }
 
-  // 触发事件，事件触发后该事件所有订阅者会被调用
+  /// 触发事件，事件触发后该事件所有订阅者会被调用
+  /// T为发布参数类型
   void emit<T>(E eventName, [T? arg]) {
     final list = _eventMap[eventName];
     if (list == null) {
