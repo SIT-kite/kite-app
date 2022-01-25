2934bcc15c767a0de996680fc38a3562ac0ce541
Zhangzqs <i@zhangzqs.cn>
2022-01-25 11:55:08 +0800

    Update: 继续补充eventBus注释

--- lib/util/event_bus.dart
+++ lib/util/event_bus.dart
@@ -9,20 +9,24 @@ class EventBus<E> {
 
   /// 添加订阅者
   void on<T>(E eventName, EventCallback<T> callback) {
+    // 如果对应事件的队列不存在，就新建一个新队列
     _eventMap[eventName] ??= <EventCallback>[];
-    final list = _eventMap[eventName]!;
-    list.add((arg) => callback(arg));
+    // 为队列追加元素，由于类型不统一，所以需要单独嵌套做类型转换
+    _eventMap[eventName]!.add((arg) => callback(arg));
   }
 
   /// 移除订阅者
   void off(E eventName, [EventCallback? f]) {
+    // 获取队列，若该事件不存在，那直接退出函数
     final list = _eventMap[eventName];
     if (list == null) {
       return;
     }
+    // 若回调为空，则删除队列所有值
     if (f == null) {
       _eventMap[eventName] = null;
     } else {
+      // 不为空时，删除特定回调
       list.remove(f);
     }
   }
@@ -34,6 +38,7 @@ class EventBus<E> {
     if (list == null) {
       return;
     }
+    // 队列队尾元素为先进元素，需要先调用
     for (final element in list.reversed) {
       element(arg);
     }
