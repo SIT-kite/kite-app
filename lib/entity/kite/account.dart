0bf435b00ed1d68a3181697af639b03a463df9e1
Zhangzqs <i@zhangzqs.cn>
2022-01-26 18:58:22 +0800

    Add: 添加用户信息json反序列化注解

--- lib/entity/kite/account.dart
+++ lib/entity/kite/account.dart
@@ -1,7 +1,13 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'account.g.dart';
+
+@JsonSerializable(createToJson: false)
 class KiteUser {
   int uid = 0;
   String account = '';
   DateTime createTime = DateTime.now();
   int role = 0;
   bool isBlock = false;
+  factory KiteUser.fromJson(Map<String, dynamic> json) => _$KiteUserFromJson(json);
 }
