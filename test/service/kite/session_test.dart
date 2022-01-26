d6640c41fbbcc127c6f743bfa12a53e3edbd51d8
Zhangzqs <i@zhangzqs.cn>
2022-01-26 22:49:25 +0800

    Update: 完善KiteSession登录测试

--- test/service/kite/session_test.dart
+++ test/service/kite/session_test.dart
@@ -1,5 +1,10 @@
 import '../mock_util.dart';
 
 void main() async {
-  test('kite login', () async {});
+  await init();
+  test('kite login', () async {
+    final profile = await SessionPool.kiteSession.login(username + '1', password);
+    Log.info(profile);
+    Log.info(StoragePool.jwt.jwtToken);
+  });
 }
