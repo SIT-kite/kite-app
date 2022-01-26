738f08f2c3ad4bbee6aae4902d24af08c0303984
Zhangzqs <i@zhangzqs.cn>
2022-01-26 15:12:41 +0800

    Add: 添加扫福活动的数据访问层的抽象接口

--- lib/dao/kite/fu.dart
+++ lib/dao/kite/fu.dart
@@ -1,12 +1,14 @@
 import 'dart:typed_data';
 
+import 'package:kite/entity/kite/fu.dart';
+
 abstract class FuDao {
   /// 上传照片
-  upload(Uint8List imageBuffer);
+  Future<UploadResultModel> upload(Uint8List imageBuffer);
 
-  /// 获取福卡列表
-  getList();
+  /// 获取我的福卡列表
+  Future<List<MyCard>> getList();
 
   /// 查询中奖结果
-  getResult();
+  Future<PraiseResult> getResult();
 }
