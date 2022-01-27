6f4c22db5b051bafd98739fc80959ee48483b90a
sunnysab <i@sunnysab.cn>
2022-01-28 01:03:13 +0800

    Fix: 修复日志模块错误.

--- lib/util/logger.dart
+++ lib/util/logger.dart
@@ -1,14 +1,18 @@
+import 'package:flutter/foundation.dart';
 import 'package:intl/intl.dart';
 
 /// 日志工具类
 class Log {
   static final dateFormat = DateFormat('yyyy-MM-dd hh:mm:ss');
+
   static String _getCaller(int deep) {
     return StackTrace.current.toString().split('\n')[deep].substring(2).split(' (')[0].trim();
   }
 
-  static void _log(String type, String message) {
-    print('${getCurrentTime()}  INFO  ${_getCaller(3)}  $message');
+  static void _log(String type, dynamic message) {
+    if (kDebugMode) {
+      print('${getCurrentTime()}\t$type\t${_getCaller(3)}  ${message.toString()}');
+    }
   }
 
   static String getCurrentTime() {
