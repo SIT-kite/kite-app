46c6f4b4548a1079112fa6cf39d7bf90c3a76931
Zhangzqs <i@zhangzqs.cn>
2022-01-27 11:08:50 +0800

    Update: 完善扫校徽活动实体定义

--- lib/entity/kite/fu.dart
+++ lib/entity/kite/fu.dart
@@ -1,26 +1,55 @@
 enum UploadResult {
-  // 无校徽
-  noLogo,
-  // 达到最大限制
+  /// 无校徽
+  noBadge,
+
+  /// 达到最大限制
   maxLimit,
-  // 没抽中
+
+  /// 没抽中
   failed,
-  // 抽中
+
+  /// 抽中
   successful,
-  // 活动结束
+
+  /// 活动结束
   outdated,
 }
 
-class UploadResultModel {
-  UploadResult result = UploadResult.noLogo;
-  int type = 0;
+enum FuType {
+  /// 无效卡片
+  noCard,
+
+  /// 爱国
+  loveCountry,
+
+  /// 富强
+  wealthy,
+
+  /// 和谐
+  harmony,
+
+  /// 友善
+  friendly,
+
+  /// 敬业
+  dedicateToWork,
 }
 
+class UploadResultModel {
+  /// 上传结果
+  UploadResult result = UploadResult.noBadge;
+
+  /// 福卡类型
+  FuType type = FuType.noCard;
+}
+
+/// 我的卡片
 class MyCard {
-  int type = 0;
+  FuType type = FuType.noCard;
   DateTime ts = DateTime.now();
 }
 
+/// 开奖信息
 class PraiseResult {
   bool hasResult = false;
   String url = '';
