e87352de27440de7b02e6b7bd4cf04938e92935d
sunnysab <i@sunnysab.cn>
2022-01-28 16:25:58 +0800

    Delete: 移除 bugly 依赖.

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bugly/flutter_bugly.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:kite/app.dart';
 import 'package:kite/global/init_util.dart';
-import 'package:universal_platform/universal_platform.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  if (UniversalPlatform.isAndroid || UniversalPlatform.isIOS) {
-    // Android/IOS使用 Bugly
-    // 使用说明 https://pub.dev/packages/flutter_bugly
-    FlutterBugly.postCatchedException(() async {
-      // 运行前初始化
-      await initBeforeRun();
-      runApp(Phoenix(child: const KiteApp()));
-      FlutterBugly.init(
-        androidAppId: "a83ed5243d",
-        iOSAppId: "7d8c9907b5",
-      );
-    });
-  } else {
-    // 桌面端和 Web 端不使用 Bugly
-    // 运行前初始化
-    await initBeforeRun();
-    runApp(Phoenix(child: const KiteApp()));
-  }
+  // 运行前初始化
+  await initBeforeRun();
+  runApp(Phoenix(child: const KiteApp()));
 }
