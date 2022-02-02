34164ddf6402bf1935fd83809dbf425d7751f10f
sunnysab <i@sunnysab.cn>
2022-02-02 18:30:33 +0800

    Add: 添加 “OA公告” 模块首页.

--- lib/page/bulletin/bulletin.dart
+++ lib/page/bulletin/bulletin.dart
@@ -1,14 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:kite/entity/bulletin.dart';
+import 'package:kite/global/session_pool.dart';
+import 'package:kite/service/bulletin.dart';
 
 class BulletinPage extends StatelessWidget {
   const BulletinPage({Key? key}) : super(key: key);
 
+  Widget _buildBulletinList() {
+    final service = BulletinService(SessionPool.ssoSession);
+    final future = service.queryBulletinListInAllCategory(1);
+
+    return FutureBuilder<List<BulletinRecord>>(
+        future: future,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasData) {
+              final records = snapshot.data!;
+              BulletinService.sortBulletinRecord(records);
+
+              final items = records
+                  .map((e) => ListTile(
+                        title: Text(e.title),
+                        subtitle: Text(e.department + ' | ' + e.dateTime.toString()),
+                      ))
+                  .toList();
+              return SingleChildScrollView(
+                  child: Column(
+                children: items,
+              ));
+            } else if (snapshot.hasError) {
+              return Center(child: Text('错误类型: ' + snapshot.error.runtimeType.toString()));
+            }
+          }
+          return const Center(child: CircularProgressIndicator());
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('OA 公告'),
-        ),
-        body: Container());
+      appBar: AppBar(
+        title: const Text('OA 公告'),
+      ),
+      body: _buildBulletinList(),
+    );
   }
 }
