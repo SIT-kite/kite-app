283ddb99c847f34232dd31b479305cf37e72674e
sunnysab <i@sunnysab.cn>
2022-02-09 11:12:26 +0800

    Update: 更新学号验证规则.

--- lib/util/validation.dart
+++ lib/util/validation.dart
@@ -1,27 +1,27 @@
 /*
- *    上应小风筝(SIT-kite)  便利校园，一步到位
- *    Copyright (C) 2022 上海应用技术大学 上应小风筝团队
+ * 上应小风筝(SIT-kite)  便利校园，一步到位
+ * Copyright (C) 2022 上海应用技术大学 上应小风筝团队
  *
- *    This program is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *    This program is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *    You should have received a copy of the GNU General Public License
- *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 // Rule of student id.
 final RegExp reStudentId = RegExp(r'^((\d{9})|(\d{6}[YGHE\d]\d{3}))$');
 
 String? studentIdValidator(String? username) {
   if (username != null && username.isNotEmpty) {
-    // When user complete his input, check it.
-    if (((username.length == 9 || username.length == 10) && !reStudentId.hasMatch(username)) || username.length > 10) {
+    // 仅允许学生学号登录, 并且屏蔽
+    if (((username.length < 9 || username.length > 10) || !reStudentId.hasMatch(username))) {
       return '学号格式不正确';
     }
   }
