4997361c27bc8e3ce5914cac567e71f9b07a9d76
Zhangzqs <i@zhangzqs.cn>
2022-02-10 15:01:02 +0800

    Update: 取消绝对路径设置

--- tool/build-tool/main.py
+++ tool/build-tool/main.py
@@ -2,10 +2,6 @@
 本工具用于便捷地运行一些编译脚本, 省去繁琐的命令行操作
 运行环境: python3.7 及以上
 """
-import sys, os
-
-sys.path.append(os.path.dirname(__file__))
-print('当前脚本运行目录: ',os.path.dirname(__file__))
 from page import show_home_menu
 
 if __name__ == '__main__':
