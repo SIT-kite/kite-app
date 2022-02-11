a7ea6655b87a2016bb00e21dbb706ef8c2babe9e
Zhangzqs <i@zhangzqs.cn>
2022-02-11 13:42:03 +0800

    Update: 重构首页内容

--- tool/build-tool/page/home.py
+++ tool/build-tool/page/home.py
@@ -1,30 +1,6 @@
 from . import *
-
-
-def run_build_runner():
-    call('flutter pub run build_runner build')
-
-
-def get_all_dependencies():
-    call('flutter pub get')
-
-
-def build_for_linux():
-    call('flutter build linux')
-
-def run_linux_release():
-    put_envs({
-      'LC_ALL':'en_US.UTF-8'
-    })
-    call('./build/linux/x64/release/bundle/kite')
-
-def build_for_windows():
-    call('flutter build windows')
-
-
-def build_for_web():
-    call('flutter build web')
-
+from .linux_menu import show_linux_menu
+from .git import show_git_menu
 
 def change_pub():
     from .change_pub import show_change_pub_menu
@@ -35,47 +11,43 @@ def android_guide():
     from .android_guide import show_guide_build_for_android
     show_guide_build_for_android()
 
-def git_pull():
-    call('git stash push',True)
-    call('git pull --rebase',True)
-    call('git stash pop')
-
 def show_home_menu():
     menu = Menu(
         title='欢迎使用小风筝App构建工具 (使用Ctrl-C可强制关闭构建过程)',
         options=[
-            MenuOption(keys=['use mirror'],
-                       title='一键换源',
+            MenuOption(keys=['mirror'],
+                       title='换源',
                        callback=change_pub),
-            MenuOption(keys=['get dependence'],
-                       title='获取所有依赖',
-                       callback=get_all_dependencies),
+
+            MenuOption(keys=['dependence'],
+                       title='依赖管理',
+                       # TODO 
+                       callback=unimplementation),
+
             MenuOption(keys=['build_runner'],
                        title='运行 build_runner (用于根据注解生成部分代码)',
-                       callback=run_build_runner),
-            MenuOption(keys=['get build'],
-                       title='获取依赖并生成代码',
-                       callback=lambda: run_functions([get_all_dependencies, run_build_runner])),
+                       callback=lambda: call('flutter pub run build_runner build',no_pause=False)),
+                       
+            MenuOption(keys=['git'],
+                       title='Git工具',
+                       callback=show_git_menu),
 
             MenuOption(keys=['android'],
-                       title='运行 android 平台构建向导',
+                       title='Android 开发',
                        callback=android_guide),
 
             MenuOption(keys=['linux'],
-                       title='构建 linux 平台应用程序',
-                       callback=build_for_linux),
-            MenuOption(keys=['run linux'],
-                     title='运行 linux release',
-                     callback=run_linux_release),
+                       title='Linux 开发',
+                       callback=show_linux_menu),
             MenuOption(keys=['windows'],
-                       title='构建 windows 平台',
-                       callback=build_for_windows),
+                       title='Windows 开发',
+                       # TODO
+                       callback=unimplementation),
+
             MenuOption(keys=['web'],
-                       title='构建 web 平台',
-                       callback=build_for_web),
-            MenuOption(keys=['pull'],
-                       title='git一键拉取最新更新',
-                       callback=git_pull),
+                       title='Web 开发',
+                       # TODO
+                       callback=unimplementation),
         ],
         backward_text='退出脚本'
     )
