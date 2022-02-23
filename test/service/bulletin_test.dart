9a50143501a54eba160b96dfdc2d54bacd04d048
Zhangzqs <i@zhangzqs.cn>
2022-02-23 23:47:08 +0800

    Fix: 移除bulletin_test的await

--- test/service/bulletin_test.dart
+++ test/service/bulletin_test.dart
@@ -9,14 +9,14 @@ void main() async {
   final session = SessionPool.ssoSession;
   final dao = BulletinService(session);
   test('test bulletin', () async {
-    final list = await dao.getAllCatalogues();
+    final list = dao.getAllCatalogues();
     Log.info(list);
     final bulletin = await dao.getBulletinDetail('pe2362', '7d227947-6dfc-11ec-9e2f-abfe89c3f6e3');
     Log.info(bulletin);
   });
 
   test('test get list', () async {
-    final list = await dao.getAllCatalogues();
+    final list = dao.getAllCatalogues();
     final firstPage = await dao.queryBulletinList(1, list[0].id);
     Log.info(firstPage);
   });
