af609d0d189c48608df6bf63105f1925aa9b6857
Zhangzqs <i@zhangzqs.cn>
2022-03-02 15:18:38 +0800

    Refactor: 反馈页面使用全局WebView页面重构

--- lib/page/feedback.dart
+++ lib/page/feedback.dart
@@ -16,10 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 import 'package:flutter/material.dart';
-import 'package:kite/component/unsupported_platform_launch.dart';
-import 'package:kite/util/url_launcher.dart';
-import 'package:universal_platform/universal_platform.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:kite/page/webview/index.dart';
 
 const String _feedbackUrl = 'https://support.qq.com/product/377648';
 
@@ -28,16 +25,9 @@ class FeedbackPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('反馈'),
-        actions: [
-          IconButton(onPressed: () => launchInBrowser(_feedbackUrl), icon: const Icon(Icons.open_in_browser)),
-        ],
-      ),
-      body: UniversalPlatform.isDesktopOrWeb
-          ? const UnsupportedPlatformUrlLauncher(_feedbackUrl)
-          : const WebView(initialUrl: _feedbackUrl),
+    return const SimpleWebViewPage(
+      _feedbackUrl,
+      fixedTitle: '反馈',
     );
   }
 }
