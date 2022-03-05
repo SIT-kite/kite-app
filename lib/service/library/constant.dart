fcb056f0ee0ebdbcefd0f1ff249dbe3df0c5c164
Zhangzqs <i@zhangzqs.cn>
2022-03-05 22:42:45 +0800

    Add: 添加图书馆借阅信息url

--- lib/service/library/constant.dart
+++ lib/service/library/constant.dart
@@ -18,12 +18,19 @@
 class Constants {
   static const homeUrl = 'http://210.35.66.106';
   static const opacUrl = homeUrl + '/opac';
+
   static const searchUrl = opacUrl + '/search';
   static const hotSearchUrl = opacUrl + '/hotsearch';
   static const apiUrl = opacUrl + '/api';
   static const bookUrl = opacUrl + '/book';
+
+  static const loanListUrl = opacUrl + '/loan';
+  static const currentLoanListUrl = loanListUrl + '/currentLoanList';
+  static const historyLoanListUrl = loanListUrl + '/historyLoanList';
+
   static const bookHoldingUrl = apiUrl + '/holding';
   static const bookHoldingPreviewsUrl = bookUrl + '/holdingPreviews';
   static const virtualBookshelfUrl = apiUrl + '/virtualBookshelf';
+
   static const bookImageInfoUrl = 'https://book-resource.dataesb.com/websearch/metares';
 }
