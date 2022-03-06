036bcf50873f7e45a71d2f31e2be5ea28fb84d33
Zhangzqs <i@zhangzqs.cn>
2022-03-06 10:49:01 +0800

    Test: 修复kiteSession测试登录错误

--- test/service/kite/session_test.dart
+++ test/service/kite/session_test.dart
@@ -3,7 +3,7 @@ import '../mock_util.dart';
 void main() async {
   await init();
   test('kite login', () async {
-    final profile = await SessionPool.kiteSession.login(username + '1', password);
+    final profile = await SessionPool.kiteSession.login(username, ssoPassword);
     Log.info(profile);
     Log.info(StoragePool.jwt.jwtToken);
   });
