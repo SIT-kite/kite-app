72d585eed29b05001bbbbb31135d86f869d40ac1
Zhangzqs <i@zhangzqs.cn>
2022-03-06 13:10:50 +0800

    Add: build-tool新增.g.dart一键清除功能

--- tool/build-tool/page/home.py
+++ tool/build-tool/page/home.py
@@ -4,6 +4,25 @@ from .git import show_git_menu
 from .android import show_android_menu
 from .mirror import show_mirror_menu
 
+def clean():
+    generated_dart_files = []
+    for root,dirs,files in os.walk('lib'):
+        if len(files) != 0:
+            for file in files:
+                dart_file = (f'{root}/{file}')
+                if dart_file.endswith('.g.dart'):
+                    generated_dart_files.append(dart_file)
+    for d in generated_dart_files:
+        print(d)
+
+    if input('将删除以上文件与Y/(N)').strip() in ['Y','y']:
+        for file in generated_dart_files:
+            print(f'删除文件: {file}')
+            os.remove(file)
+    else:
+        print('取消删除')
+    pause()
+
 def show_home_menu():
     menu = Menu(
         title='欢迎使用小风筝App构建工具 (使用Ctrl-C可强制关闭构建过程)',
@@ -12,15 +31,19 @@ def show_home_menu():
                        title='换源',
                        callback=show_mirror_menu),
 
+            MenuOption(keys=['clean'],
+                       title='清理',
+                       callback=clean),
+
             MenuOption(keys=['dependence'],
                        title='依赖管理',
-                       # TODO 
+                       # TODO
                        callback=unimplementation),
 
             MenuOption(keys=['build_runner'],
                        title='运行 build_runner (用于根据注解生成部分代码)',
                        callback=lambda: call('flutter pub run build_runner build',no_pause=False)),
-                       
+
             MenuOption(keys=['git'],
                        title='Git工具',
                        callback=show_git_menu),
