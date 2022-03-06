24cae85fcbeaa159d2eabfe82a31b49dac3679dc
Zhangzqs <i@zhangzqs.cn>
2022-03-06 10:48:04 +0800

    Test: 添加两条新字段 ssoPassword, libraryPassword

--- test/service/mock_util.dart
+++ test/service/mock_util.dart
@@ -9,7 +9,10 @@ export 'package:kite/util/logger.dart';
 
 // 这里填写用于测试的用户名密码
 const String username = '';
-const String password = '';
+const String ssoPassword = '';
+
+// 图书馆密码
+const String libraryPassword = '';
 
 // 如果需要代理，请在这里设置
 const String proxy = '';
@@ -26,7 +29,7 @@ Future<void> init() async {
 
 /// 如果需要登录，调用该函数
 Future<void> login() async {
-  await SessionPool.ssoSession.login(username, password);
+  await SessionPool.ssoSession.login(username, ssoPassword);
 }
 
 /// 图书馆登陆
