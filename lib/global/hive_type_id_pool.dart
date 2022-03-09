b9bb152f6b0b025956650cae477235f397016b52
Zhangzqs <i@zhangzqs.cn>
2022-03-09 14:19:52 +0800

    Refactor: 注释多用户实体hive id

--- lib/global/hive_type_id_pool.dart
+++ lib/global/hive_type_id_pool.dart
@@ -18,7 +18,7 @@
 /// 存放所有 hive 的自定义类型的typeId
 class HiveTypeIdPool {
   static const librarySearchHistoryItem = 1;
-  static const authItem = 2;
+  static const authItem = 2; // 改为单用户后已不再使用该id
   static const weatherItem = 3;
   static const reportHistoryItem = 4;
   static const balanceItem = 5;
