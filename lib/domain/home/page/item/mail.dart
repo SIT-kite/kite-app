02c3abd3f0858739bf89eefbbb165e8e390475a4
Zhangzqs <i@zhangzqs.cn>
2022-03-09 23:51:45 +0800

    Remove: 删除不必要的global/event_bus.dart

--- lib/domain/home/page/item/mail.dart
+++ lib/domain/home/page/item/mail.dart
@@ -16,7 +16,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 import 'package:flutter/material.dart';
-import 'package:kite/global/event_bus.dart';
 import 'package:kite/global/global.dart';
 
 import 'index.dart';
