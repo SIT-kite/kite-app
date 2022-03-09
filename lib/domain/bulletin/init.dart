b1f1c5b8fab7c4acebf775e365226f97cb4f6fb8
Zhangzqs <i@zhangzqs.cn>
2022-03-09 18:30:10 +0800

    Refactor: 使Bulletin依赖倒置

--- lib/domain/bulletin/init.dart
+++ lib/domain/bulletin/init.dart
@@ -1,3 +1,4 @@
+import 'package:kite/abstract/abstract_session.dart';
 import 'package:kite/session/sso/index.dart';
 
 import 'dao/bulletin.dart';
@@ -6,7 +7,7 @@ import 'service/bulletin.dart';
 class BulletinInitializer {
   static late BulletinDao bulletin;
   static late SsoSession session;
-  static init(SsoSession session) {
+  static init({required ASession ssoSession}) {
     BulletinInitializer.session = session;
     bulletin = BulletinService(session);
   }
