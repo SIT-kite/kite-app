1c8dac039da923cebbbdd162b5d0e64b517302a8
Zhangzqs <i@zhangzqs.cn>
2022-03-10 15:00:01 +0800

    Add: 添加Range工具

---
+++ test/util/range_test.dart
@@ -0,0 +1,12 @@
+import 'package:kite/util/range.dart';
+
+import '../service/mock_util.dart';
+
+void main() {
+  test('range', () {
+    expect(range(10).toList(), List.generate(10, (index) => index));
+    expect(range(10.0).toList(), List.generate(10, (index) => index.toDouble()));
+    expect(range(4, 7).toList(), [4, 5, 6]);
+    expect(range(0, 10, 2).toList(), [0, 2, 4, 6, 8]);
+  });
+}
