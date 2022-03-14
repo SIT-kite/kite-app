d1a8f489cc92b6c88f63304953c6be6dae910c47
Zhangzqs <i@zhangzqs.cn>
2022-03-14 19:46:41 +0800

    Add: 添加姓名获取测试

--- test/service/sso_test.dart
+++ test/service/sso_test.dart
@@ -1,4 +1,5 @@
 import 'package:beautiful_soup_dart/beautiful_soup.dart';
+import 'package:kite/feature/initializer_index.dart';
 import 'package:kite/global/global.dart';
 
 import 'mock_util.dart';
@@ -8,12 +9,16 @@ void main() async {
   await login();
   var session = Global.ssoSession;
   test('test login', () async {
-    var index = await session.get('https://myportal.sit.edu.cn/');
-    var list = BeautifulSoup(index.data)
+    final index = await session.get('https://myportal.sit.edu.cn/');
+    final list = BeautifulSoup(index.data)
         .find('div', class_: 'composer')!
         .findAll('li')
         .map((e) => e.text.trim().replaceAll('\n', '').replaceAll(' ', ''))
         .toList();
     expect(list[0].contains('姓名'), true);
+  });
+  test('get person name', () async {
+    final name = await LoginInitializer.authServerService.getPersonName();
+    print('姓名: $name');
   });
 }
