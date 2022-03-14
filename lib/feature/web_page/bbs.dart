c193d378d45a48554b3eea6a54e8ea6bf3e0de9f
Zhangzqs <i@zhangzqs.cn>
2022-03-14 20:18:51 +0800

    Update: bbs支持注入登录态

--- lib/feature/web_page/bbs.dart
+++ lib/feature/web_page/bbs.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:kite/feature/web_page/webview/page/index.dart';
+import 'package:kite/setting/init.dart';
+
+import 'webview/page/index.dart';
+
+const _bbsUrl = 'https://support.qq.com/products/386124';
 
 class BbsPage extends StatelessWidget {
   const BbsPage({Key? key}) : super(key: key);
 
+  String _getNickname() {
+    final srcName = SettingInitializer.auth.personName!;
+    return srcName[0] + '同学';
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const SimpleWebViewPage(
-      initialUrl: 'https://support.qq.com/products/386124',
+    return SimpleWebViewPage(
+      initialUrl: _bbsUrl,
       postData: {
-        'openid': 'tucao_123',
-        'nickname': 'tucao_test',
+        'openid': SettingInitializer.auth.currentUsername!,
+        'nickname': _getNickname(),
         'avatar': 'https://txc.qq.com/static/desktop/img/products/def-product-logo.png',
       },
       fixedTitle: '问答',
