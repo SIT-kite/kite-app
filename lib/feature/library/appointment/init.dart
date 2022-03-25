0c8a138c21a31c5d83ee8b58479d05248e4477de
Zhangzqs <i@zhangzqs.cn>
2022-03-25 23:42:16 +0800

    Update: 图书馆预约模块初始化器添加service的初始化

--- lib/feature/library/appointment/init.dart
+++ lib/feature/library/appointment/init.dart
@@ -1,10 +1,14 @@
+import 'package:kite/feature/library/appointment/dao.dart';
+import 'package:kite/feature/library/appointment/service.dart';
 import 'package:kite/session/kite_session.dart';
 
 class LibraryAppointmentInitializer {
   static late KiteSession kiteSession;
+  static late AppointmentDao appointmentService;
   static void init({
     required KiteSession kiteSession,
   }) {
     LibraryAppointmentInitializer.kiteSession = kiteSession;
+    appointmentService = AppointmentService(kiteSession);
   }
 }
