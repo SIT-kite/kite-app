bb5bb429fdd2aab5080a5dd07c877c95c85f7ca6
Zhangzqs <i@zhangzqs.cn>
2022-03-26 17:31:54 +0800

    Add: 补充图书馆预约dao

--- lib/feature/library/appointment/dao.dart
+++ lib/feature/library/appointment/dao.dart
@@ -9,10 +9,13 @@ abstract class AppointmentDao {
   Future<List<PeriodStatusRecord>> getPeriodStatus(DateTime dateTime);
 
   /// 查询自己的所有预约记录
-  Future<List<ApplicationRecord>> getApplication({int? period, String? username});
+  Future<List<ApplicationRecord>> getApplication({int? period, String? username, DateTime? date});
 
   /// 查询预约凭证
   Future<String> getApplicationCode(int applyId);
+
+  /// 获取RSA加密公钥
+  Future<String> getRsaPublicKey();
 
   /// 申请座位
   /// 返回applyId
@@ -23,4 +26,7 @@ abstract class AppointmentDao {
 
   /// 取消预约状态
   Future<void> cancelApplication(int applyId);
+
+  /// 获取当前开放场次
+  Future<CurrentPeriodResponse?> getCurrentPeriod();
 }
