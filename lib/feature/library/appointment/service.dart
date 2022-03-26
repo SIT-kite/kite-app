d53e991ce19f02ed37b3f7d31ef655d78c942c53
Zhangzqs <i@zhangzqs.cn>
2022-03-26 17:33:14 +0800

    Update: 补充图书馆预约的service实现

--- lib/feature/library/appointment/service.dart
+++ lib/feature/library/appointment/service.dart
@@ -1,3 +1,4 @@
+import 'package:intl/intl.dart';
 import 'package:kite/abstract/abstract_service.dart';
 import 'package:kite/abstract/abstract_session.dart';
 import 'package:kite/feature/library/appointment/dao.dart';
@@ -27,16 +28,20 @@ class AppointmentService extends AService implements AppointmentDao {
   }
 
   @override
-  Future<List<ApplicationRecord>> getApplication({int? period, String? username}) async {
-    Map<String, String>? queryParameters;
-    if (period != null && username == null) {
+  Future<List<ApplicationRecord>> getApplication({
+    int? period,
+    String? username,
+    DateTime? date,
+  }) async {
+    Map<String, String> queryParameters = {};
+    if (period != null) {
       queryParameters = {'period': period.toString()};
     }
-    if (period == null && username != null) {
+    if (username != null) {
       queryParameters = {'user': username};
     }
-    if (queryParameters == null) {
-      throw Exception('查询字段只能period,username二选一');
+    if (date != null) {
+      queryParameters['date'] = DateFormat('yyyyMMdd').format(date);
     }
     final response = await session.get('$_application/', queryParameters: queryParameters);
     List raw = response.data;
@@ -65,5 +70,20 @@ class AppointmentService extends AService implements AppointmentDao {
   @override
   Future<void> updateApplication(int applyId, int status) async {
     await session.patch('$_application/$applyId', data: {'status': status});
+  }
+
+  @override
+  Future<CurrentPeriodResponse?> getCurrentPeriod() async {
+    final response = await session.get('$_library/current');
+    if (response.data == null) {
+      return null;
+    }
+    return CurrentPeriodResponse.fromJson(response.data);
+  }
+
+  @override
+  Future<String> getRsaPublicKey() async {
+    final response = await session.get('$_library/publicKey');
+    return response.data;
   }
 }
