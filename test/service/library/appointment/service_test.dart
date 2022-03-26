4495b93074c2759333bf5526118296a58bc78ce5
Zhangzqs <i@zhangzqs.cn>
2022-03-26 17:33:24 +0800

    Update: 补充图书馆预约的service测试

--- test/service/library/appointment/service_test.dart
+++ test/service/library/appointment/service_test.dart
@@ -11,23 +11,42 @@ void main() async {
     final result = await service.apply(period);
     Log.info(result);
   });
+
   test('cancelApplication', () async {
     await service.cancelApplication(2203261);
   });
-  test('getApplication', () async {
+  test('getApplicationBySelf', () async {
     // 获取自己的所有申请
     final application = await service.getApplication(username: username);
     Log.info(application);
   });
+
+  test('getApplicationByPeriod', () async {
+    // 获取自己的所有申请
+    final application = await service.getApplication(period: 2203261);
+    Log.info(application);
+  });
+
+  test('getApplicationBySelfDate', () async {
+    // 获取今天的所有申请
+    final application = await service.getApplication(
+      date: DateTime.now(),
+      username: username,
+    );
+    Log.info(application);
+  });
+
   test('getApplicationCode', () async {
-    int applyId = 2203262;
+    int applyId = 15;
     final code = await service.getApplicationCode(applyId);
     Log.info(code);
   });
+
   test('getNotice', () async {
     final notice = await service.getNotice();
     Log.info(notice);
   });
+
   test('getPeriodStatus', () async {
     final today = DateTime.now();
     final tomorrow = today.add(const Duration(days: 1));
@@ -36,9 +55,19 @@ void main() async {
     final status1 = await service.getPeriodStatus(tomorrow);
     Log.info('明日时间: $status1');
   });
+
   test('updateApplication', () async {
     int applyId = 2203262;
     int status = 0;
     await service.updateApplication(applyId, status);
+  });
+
+  test('getCurrentPeriod', () async {
+    final currentPeriod = await service.getCurrentPeriod();
+    Log.info('current period: $currentPeriod');
+  });
+  test('getRsaPublicKey', () async {
+    final rsaPubKey = await service.getRsaPublicKey();
+    Log.info('rsa public key: \n$rsaPubKey');
   });
 }
