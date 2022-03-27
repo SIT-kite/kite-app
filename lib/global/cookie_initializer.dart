396383a7201fff0703ac37ceb600573350622f44
Zhangzqs <i@zhangzqs.cn>
2022-03-28 01:41:01 +0800

    Update: 将cookie存内存中

--- lib/global/cookie_initializer.dart
+++ lib/global/cookie_initializer.dart
@@ -1,12 +1,12 @@
 import 'package:cookie_jar/cookie_jar.dart';
-import 'package:path_provider/path_provider.dart';
 
 class CookieInitializer {
   static Future<CookieJar> init({String path = '/kite1/cookies/'}) async {
-    final String homeDirectory = (await getApplicationDocumentsDirectory()).path;
-    final FileStorage cookieStorage = FileStorage(homeDirectory + path);
-    // 初始化持久化的 cookieJar
-    final cookieJar = PersistCookieJar(storage: cookieStorage);
+    // final String homeDirectory = (await getApplicationDocumentsDirectory()).path;
+    // final FileStorage cookieStorage = FileStorage(homeDirectory + path);
+    // // 初始化持久化的 cookieJar
+    // final cookieJar = PersistCookieJar(storage: cookieStorage);
+    final cookieJar = DefaultCookieJar();
     return cookieJar;
   }
 }
