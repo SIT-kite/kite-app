2cd0d09e2398aa8c9cfc495d30352e565f27fa00
Zhangzqs <i@zhangzqs.cn>
2022-03-30 20:08:42 +0800

    Add: 添加全局浏览器页面

---
+++ lib/feature/web_page/browser.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:kite/component/webview_page.dart';
+
+class BrowserPage extends StatelessWidget {
+  final String initialUrl;
+  const BrowserPage(this.initialUrl, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SimpleWebViewPage(
+      initialUrl: initialUrl,
+    );
+  }
+}
