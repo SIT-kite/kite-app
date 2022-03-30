8e660fd3ebd8474f185cc1ba7e0513c43aa72f7d
Zhangzqs <i@zhangzqs.cn>
2022-03-30 20:33:44 +0800

    Add: 添加启动器工具类

---
+++ lib/util/launcher.dart
@@ -0,0 +1,34 @@
+import 'package:kite/util/rule.dart';
+
+typedef onLaunchCallback = void Function(String);
+
+class LaunchScheme {
+  final Rule<String> launchRule;
+  final onLaunchCallback onLaunch;
+
+  const LaunchScheme({
+    required this.launchRule,
+    required this.onLaunch,
+  });
+}
+
+class Launcher {
+  List<LaunchScheme> schemes;
+  onLaunchCallback? onNotFound;
+  Launcher({
+    this.schemes = const [],
+    this.onNotFound,
+  });
+
+  void launch(String schemeText) {
+    for (final scheme in schemes) {
+      if (scheme.launchRule.accept(schemeText)) {
+        scheme.onLaunch(schemeText);
+        return;
+      }
+      if (onNotFound != null) {
+        onNotFound!(schemeText);
+      }
+    }
+  }
+}
