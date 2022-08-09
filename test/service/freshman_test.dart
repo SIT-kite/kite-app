566b155fb8fe0b0ac9148709359dcb44efd70b8d
zzq <i@zhangzqs.cn>
2022-08-09 18:44:33 +0800

    Add: 添加Freshman的Service测试文件

--- test/service/freshman_test.dart
+++ test/service/freshman_test.dart
@@ -1,5 +1,4 @@
 import 'package:kite/feature/freshman/dao.dart';
-import 'package:kite/feature/freshman/entity.dart';
 import 'package:kite/feature/freshman/init.dart';
 import 'package:kite/mock/index.dart';
 import 'package:kite/setting/init.dart';
@@ -10,8 +9,31 @@ void main() async {
   SettingInitializer.auth
     ..freshmanAccount = '216072111'
     ..freshmanSecret = '134629';
-  test('test freshman service', () async {
-    FreshmanInfo info = await freshmanDao.getInfo();
-    Log.info(info.name);
+  test('test freshman getInfo', () async {
+    Log.info(await freshmanDao.getInfo());
+  });
+  test('test freshman postAnalysisLog', () async {
+    Log.info(await freshmanDao.getInfo());
+    await freshmanDao.update(visible: false);
+    Log.info(await freshmanDao.getInfo());
+  });
+  test('test freshman getRoommates', () async {
+    final roommates = await freshmanDao.getRoommates();
+    Log.info(roommates);
+  });
+  test('test freshman getFamiliars', () async {
+    final familiars = await freshmanDao.getFamiliars();
+    Log.info(familiars);
+  });
+  test('test freshman getClassmates', () async {
+    final classmates = await freshmanDao.getClassmates();
+    Log.info(classmates);
+  });
+  test('test freshman getAnalysis', () async {
+    final analysis = await freshmanDao.getAnalysis();
+    Log.info(analysis);
+  });
+  test('test freshman postAnalysisLog', () async {
+    await freshmanDao.postAnalysisLog();
   });
 }
