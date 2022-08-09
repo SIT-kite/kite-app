4e88d56b49607d306931b5fc72b3319210c0b162
zzq <i@zhangzqs.cn>
2022-08-09 15:55:13 +0800

    Add: 添加新生的所有实体

--- lib/feature/freshman/entity.dart
+++ lib/feature/freshman/entity.dart
@@ -17,6 +17,82 @@ class FreshmanInfo {
   String counselorTel = '';
   bool visible = false;
   FreshmanInfo();
-  factory FreshmanInfo.fromJson(Map<String, dynamic> json) =>
-      _$FreshmanInfoFromJson(json);
+  factory FreshmanInfo.fromJson(Map<String, dynamic> json) => _$FreshmanInfoFromJson(json);
+}
+
+@JsonSerializable()
+class Contact {
+  String? wechat;
+  String? qq;
+  String? phone;
+
+  Contact();
+  factory Contact.fromJson(Map<String, dynamic> json) => _$ContactFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ContactToJson(this);
+}
+
+@JsonSerializable()
+class Roommate {
+  String college = '';
+  String major = '';
+  String name = '';
+  String province = '';
+  String bed = '';
+  String lastSeen = '';
+  String avatar = '';
+  Contact contact = Contact();
+  Roommate();
+  factory Roommate.fromJson(Map<String, dynamic> json) => _$RoommateFromJson(json);
+}
+
+@JsonSerializable()
+class Familiar {
+  String name = '';
+  String college = '';
+  String city = '';
+  String gender = '';
+  String avatar = '';
+  String lastSeen = '';
+  Contact contact = Contact();
+  Familiar();
+  factory Familiar.fromJson(Map<String, dynamic> json) => _$FamiliarFromJson(json);
+}
+
+@JsonSerializable()
+class Classmate {
+  String college = '';
+  String major = '';
+  String name = '';
+  String province = '';
+  String building = '';
+  String room = '';
+  String bed = '';
+  String gender = '';
+  String lastSeen = '';
+  String avatar = '';
+  Contact contact = Contact();
+  Classmate();
+  factory Classmate.fromJson(Map<String, dynamic> json) => _$ClassmateFromJson(json);
+}
+
+@JsonSerializable()
+class AnalysisMajor {
+  int total = 0;
+  int boys = 0;
+  int girls = 0;
+
+  AnalysisMajor();
+  factory AnalysisMajor.fromJson(Map<String, dynamic> json) => _$AnalysisMajorFromJson(json);
+}
+
+@JsonSerializable()
+class Analysis {
+  int sameName = 0;
+  int sameCity = 0;
+  int sameHighSchool = 0;
+  int collegeCount = 0;
+  AnalysisMajor major = AnalysisMajor();
+  Analysis();
+  factory Analysis.fromJson(Map<String, dynamic> json) => _$AnalysisFromJson(json);
 }
