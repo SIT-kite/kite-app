d610f91c627cb81500aaace8471641f90f6902aa
zzq <i@zhangzqs.cn>
2022-08-11 18:26:37 +0800

    Add: 添加分析风筝页面

--- lib/feature/freshman/page/analysis.dart
+++ lib/feature/freshman/page/analysis.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:kite/component/future_builder.dart';
+import 'package:kite/feature/freshman/entity.dart';
 
 import '../dao.dart';
 import '../init.dart';
+import 'component/basic_info.dart';
 
 class FreshmanAnalysisPage extends StatefulWidget {
   const FreshmanAnalysisPage({Key? key}) : super(key: key);
@@ -12,12 +15,30 @@ class FreshmanAnalysisPage extends StatefulWidget {
 
 class _FreshmanAnalysisPageState extends State<FreshmanAnalysisPage> {
   final FreshmanDao freshmanDao = FreshmanInitializer.freshmanDao;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Row(
-        children: [Text('ACV')],
-      ),
+    return MyFutureBuilder<List<dynamic>>(
+      future: Future.wait([freshmanDao.getAnalysis(), freshmanDao.getInfo()]),
+      builder: (context, data) {
+        return _buildBody(context, data[0], data[1]);
+      },
+    );
+  }
+
+  Widget _buildBody(BuildContext context, Analysis data, FreshmanInfo info) {
+    return BasicInfoPageWidget(
+      name: info.name,
+      college: info.college,
+      infoItems: [
+        InfoItem(Icons.person, "同名人数", '${data.sameName} 人'),
+        InfoItem(Icons.location_city, "来自同一个城市的人数", '${data.sameCity} 人'),
+        InfoItem(Icons.school, "来自同一个高中的人数", '${data.sameHighSchool} 人'),
+        InfoItem(Icons.school, "学院总人数", '${data.collegeCount} 人'),
+        InfoItem(Icons.school, "专业总人数", '${data.major.total} 人'),
+        InfoItem(Icons.male, "专业男生人数", '${data.major.boys} 人'),
+        InfoItem(Icons.female, "专业女生人数", '${data.major.girls} 人'),
+      ],
     );
   }
 }
