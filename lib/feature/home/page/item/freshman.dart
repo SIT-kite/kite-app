aa7eac7cf8d9834dc575569f58d49f17ec017edf
zzq <i@zhangzqs.cn>
2022-08-15 02:53:02 +0800

    Update: 迎新入口是否隐藏的业务逻辑

--- lib/feature/home/page/item/freshman.dart
+++ lib/feature/home/page/item/freshman.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:kite/setting/dao/index.dart';
+import 'package:kite/setting/init.dart';
 
 import 'index.dart';
 
@@ -8,6 +10,18 @@ class FreshmanItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final userType = SettingInitializer.auth.userType!;
+    // 老师根本不会显示这个列表项
+    // 所以只考虑正式学生的情况
+    if (userType != UserType.freshman) {
+      // 正式学生，获取学号
+      final username = SettingInitializer.auth.currentUsername!;
+      // 取今年的后两位，若今年的后两位大于学号前两位
+      // 说明已经不是新生了
+      if (DateTime.now().year % 100 > int.parse(username.substring(0, 2))) {
+        return Container();
+      }
+    }
     return HomeFunctionButton(
       route: '/freshman',
       iconWidget: Icon(Icons.people, size: 30.h, color: Theme.of(context).primaryColor),
