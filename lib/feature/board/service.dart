f1b16e94c9ced91b1dd9b2ba23a8d07b1d78b6c5
sunnysab <i@sunnysab.cn>
2022-08-20 11:34:45 +0800

    Fix: "照片墙" 图片列表分页.

--- lib/feature/board/service.dart
+++ lib/feature/board/service.dart
@@ -30,7 +30,7 @@ class BoardService extends AService implements BoardDao {
 
   @override
   Future<List<PictureSummary>> getPictureList({int page = 1, int count = 20}) async {
-    final response = await session.get('$_boardUrl/');
+    final response = await session.get('$_boardUrl/?index=$page&count=$count');
     final List pictureList = response.data;
 
     List<PictureSummary> result = pictureList.map((e) => PictureSummary.fromJson(e)).toList();
