1ea07d6143dd995a01496e617c0c75cf0e0c4e5f
sunnysab <i@sunnysab.cn>
2022-08-29 09:56:20 +0800

    Add: Ventusky 天气源.
    ref: https://www.ventusky.com/?p=31.046;121.773;10&l=rain-1h

--- lib/feature/home/page/greeting.dart
+++ lib/feature/home/page/greeting.dart
@@ -18,19 +18,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:kite/feature/kite/entity/weather.dart';
+import 'package:kite/feature/web_page/weather.dart';
 import 'package:kite/global/global.dart';
 import 'package:kite/storage/init.dart';
-import 'package:kite/util/url_launcher.dart';
 import 'package:kite/util/user.dart';
 
 /// 计算入学时间, 默认按 9 月 1 日开学来算. 年份 entranceYear 是完整的年份, 如 2018.
 int _calcStudyDays(int entranceYear) {
   int days = DateTime.now().difference(DateTime(entranceYear, 9, 1)).inDays;
   return days;
-}
-
-String _getWeatherUrl(String location) {
-  return 'https://widget-page.qweather.net/h5/index.html?md=0123456&bg=1&lc=$location&key=f96261862c08497c90c0dea53467f511';
 }
 
 class GreetingWidget extends StatefulWidget {
@@ -79,8 +75,7 @@ class _GreetingWidgetState extends State<GreetingWidget> {
     return GestureDetector(
       onTap: () {
         final title = '${_getCampusName()}天气';
-        final url = _getWeatherUrl(campus == 1 ? '101021000' : '101021200'); // TODO: 支持长桥校区和梅陇校区
-        launchUrlInBuiltinWebView(context, url, fixedTitle: title);
+        Navigator.of(context).push(MaterialPageRoute(builder: (context) => WeatherPage(campus, title: title)));
       },
       child: SvgPicture.asset('assets/weather/$iconCode.svg',
           width: 60, height: 60, fit: BoxFit.fill, color: Colors.white),
