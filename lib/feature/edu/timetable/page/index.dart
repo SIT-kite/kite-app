a5bf2e46ac42387c462dcc3e6bac1ae8f3e4a299
zzq <i@zhangzqs.cn>
2022-09-03 16:11:04 +0800

    Add: 课表导出日历支持导出到URL

--- lib/feature/edu/timetable/page/index.dart
+++ lib/feature/edu/timetable/page/index.dart
@@ -15,17 +15,23 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+import 'dart:io';
+
+import 'package:catcher/catcher.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:kite/feature/edu/timetable/cache.dart';
 import 'package:kite/feature/edu/timetable/init.dart';
 import 'package:kite/feature/edu/timetable/page/component/daily_and_weekly.dart';
-import 'package:kite/feature/edu/timetable/util.dart';
 import 'package:kite/route.dart';
 import 'package:kite/util/alert_dialog.dart';
 import 'package:kite/util/flash.dart';
 import 'package:kite/util/logger.dart';
 
 import '../entity.dart';
+import '../util.dart';
 
 class TimetablePage extends StatefulWidget {
   const TimetablePage({Key? key}) : super(key: key);
@@ -84,12 +90,69 @@ class _TimetablePageState extends State<TimetablePage> {
     super.initState();
   }
 
+  void _exportByUrl() async {
+    const url = 'http://localhost:8081';
+    HttpServer? server;
+    try {
+      server = await HttpServer.bind(InternetAddress.anyIPv6, 8081, shared: true);
+
+      Log.info('HTTP服务启动成功');
+      server.listen((HttpRequest request) {
+        request.response.write(convertTableToIcs(meta!, courses));
+        request.response.close();
+      });
+
+      if (!mounted) return;
+
+      await showAlertDialog(
+        context,
+        title: '已生成链接',
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextButton(
+              onPressed: () async {
+                await Clipboard.setData(const ClipboardData(text: url));
+                EasyLoading.showSuccess('成功复制到剪切板');
+              },
+              child: const Text(url),
+            ),
+            const Text('注意：点击可复制链接, 关闭本对话框后链接将失效'),
+          ],
+        ),
+        actionTextList: ['关闭'],
+      );
+    } catch (e, st) {
+      Catcher.reportCheckedError(e, st);
+      return;
+    } finally {
+      server?.close();
+      Log.info('HTTP服务已关闭');
+    }
+  }
+
   void _onExport() {
     if (courses.isEmpty) {
       showBasicFlash(context, const Text('你咋没课呢？？'));
       return;
     }
-    exportTimetableToCalendar(meta!, courses);
+    showAlertDialog(context,
+        title: '请选择导出方式',
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ElevatedButton(
+              onPressed: () => exportTimetableToCalendar(meta!, courses),
+              child: const Text('导出至文件'),
+            ),
+            SizedBox(height: 10.h),
+            ElevatedButton(
+              onPressed: _exportByUrl,
+              child: const Text('导出为URL'),
+            ),
+          ],
+        ),
+        actionWidgetList: [TextButton(onPressed: () {}, child: const Text('关闭对话框'))]);
   }
 
   /// 根据本地缓存刷新课表
