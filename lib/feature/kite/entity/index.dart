5053f554f02dd5b74c1fc2b3a50bacf682ebc061
zzq <i@zhangzqs.cn>
2022-09-04 12:09:04 +0800

    Refactor: 移动小风筝账户实体位置

--- lib/feature/kite/entity/index.dart
+++ lib/feature/kite/entity/index.dart
@@ -16,6 +16,5 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-export 'account.dart';
 export 'electricity.dart';
 export 'weather.dart';
--- lib/session/kite_session.dart
+++ lib/session/kite_session.dart
@@ -18,11 +18,30 @@
 import 'dart:async';
 
 import 'package:dio/dio.dart';
+import 'package:json_annotation/json_annotation.dart';
 import 'package:kite/abstract/abstract_session.dart';
 import 'package:kite/storage/init.dart';
 import 'package:kite/util/logger.dart';
 
-import '../feature/kite/entity/account.dart';
+part 'kite_session.g.dart';
+
+@JsonSerializable(createToJson: false)
+class KiteUser {
+  int uid = 0;
+  String account = '';
+  DateTime createTime = DateTime.now();
+  int role = 0;
+  bool isBlock = false;
+
+  KiteUser();
+
+  factory KiteUser.fromJson(Map<String, dynamic> json) => _$KiteUserFromJson(json);
+
+  @override
+  String toString() {
+    return 'KiteUser{uid: $uid, account: $account, createTime: $createTime, role: $role, isBlock: $isBlock}';
+  }
+}
 
 const String _baseUrl = 'https://kite.sunnysab.cn/api/v2';
 
