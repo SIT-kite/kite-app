392c09c314956c048a06f80669528d6ae8b4da23
Liplum <Li_plum@outlook.com>
2022-12-02 20:11:49 +0800

    Fixed Network Tool Page.

--- lib/module/connectivity/page/index.dart
+++ lib/module/connectivity/page/index.dart
@@ -20,8 +20,10 @@ import 'package:check_vpn_connection/check_vpn_connection.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:kite/design/user_widgets/dialog.dart';
+import 'package:rettulf/rettulf.dart';
 
 import '../init.dart';
 import '../service/network.dart';
@@ -42,7 +44,8 @@ class _ConnectivityPageState extends State<ConnectivityPage> {
   void initState() {
     super.initState();
 
-    checkConnectivityFuture = ConnectivityInit.ssoSession.checkConnectivity().then((value) {
+    checkConnectivityFuture =
+        ConnectivityInit.ssoSession.checkConnectivity().then((value) {
       Log.info('当前是否连接校园网：$value');
       if (!mounted) return;
       setState(() => isConnected = value);
@@ -52,20 +55,21 @@ class _ConnectivityPageState extends State<ConnectivityPage> {
   Widget buildFigure(BuildContext context) {
     final Color primaryColor = Theme.of(context).primaryColor;
     final Color color = isConnected ? primaryColor : Colors.grey;
-    return SvgPicture.asset('assets/connectivity/not-available.svg', width: 260, height: 260, color: color);
+    return SvgPicture.asset('assets/connectivity/not-available.svg',
+        width: 240, height: 240, color: color);
   }
 
-  Widget buildConnectedBlock() {
+  List<Widget> buildConnectedBlock() {
     final style = Theme.of(context).textTheme.bodyText1;
 
-    Widget buildConnectedByProxy() => Text(
+    late Widget buildConnectedByProxy = Text(
         '${i18n.connectivityConnectedByVpn}\n'
         '${i18n.address}：${Kv.network.proxy}',
         textAlign: TextAlign.center,
         style: style);
 
-    Widget buildConnectedByVpnBlock() =>
-        Text(i18n.connectivityConnectedByVpn, textAlign: TextAlign.center, style: style);
+    Widget buildConnectedByVpnBlock() => Text(i18n.connectivityConnectedByVpn,
+        textAlign: TextAlign.center, style: style);
     Widget buildConnectedByWlanBlock() {
       return FutureBuilder(
         future: Network.checkStatus(),
@@ -96,20 +100,22 @@ class _ConnectivityPageState extends State<ConnectivityPage> {
     }
 
     if (Kv.network.useProxy) {
-      return buildConnectedByProxy();
+      return [buildConnectedByProxy];
     }
-    return FutureBuilder(
-      future: CheckVpnConnection.isVpnActive(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          final isVpnActive = snapshot.data!;
-          if (false == isVpnActive) {
-            return buildConnectedByWlanBlock();
+    return [
+      FutureBuilder(
+        future: CheckVpnConnection.isVpnActive(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            final isVpnActive = snapshot.data!;
+            if (false == isVpnActive) {
+              return buildConnectedByWlanBlock();
+            }
           }
-        }
-        return buildConnectedByVpnBlock();
-      },
-    );
+          return buildConnectedByVpnBlock();
+        },
+      )
+    ];
   }
 
   Widget buildErrorText() {
@@ -123,50 +129,37 @@ class _ConnectivityPageState extends State<ConnectivityPage> {
     );
   }
 
-  Widget buildDisconnectedBlock() {
-    return Column(
-      children: [
-        buildErrorText(),
-        const SizedBox(height: 20),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 40,
-              child: ElevatedButton(
-                onPressed: AppSettings.openWIFISettings,
-                child: i18n.openWlanSettingsBtn.txt,
-              ),
-            ),
-            const SizedBox(width: 20),
-            SizedBox(
-              height: 40,
-              child: ElevatedButton(
-                child: i18n.launchEasyConnectBtn.txt,
-                onPressed: () async {
-                  final launched = await GlobalLauncher.launch('sangfor://easyconnect');
-                  if (!launched) {
-                    if (!mounted) return;
-                    final confirm = await context.showRequest(
-                        title: i18n.easyconnectLaunchFailed,
-                        desc: i18n.easyconnectLaunchFailedDesc,
-                        yes: i18n.download,
-                        no: i18n.notNow,
-                        highlight: true);
-                    if (confirm == true) {
-                      await GlobalLauncher.launch(R.easyConnectDownloadUrl);
-                    }
+  List<Widget> buildDisconnectedBlock() {
+    return [
+      buildErrorText(),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ElevatedButton(
+            onPressed: AppSettings.openWIFISettings,
+            child: i18n.openWlanSettingsBtn.txt,
+          ),
+          ElevatedButton(
+              child: i18n.launchEasyConnectBtn.txt,
+              onPressed: () async {
+                final launched =
+                    await GlobalLauncher.launch('sangfor://easyconnect');
+                if (!launched) {
+                  if (!mounted) return;
+                  final confirm = await context.showRequest(
+                      title: i18n.easyconnectLaunchFailed,
+                      desc: i18n.easyconnectLaunchFailedDesc,
+                      yes: i18n.download,
+                      no: i18n.notNow,
+                      highlight: true);
+                  if (confirm == true) {
+                    await GlobalLauncher.launch(R.easyConnectDownloadUrl);
                   }
-                },
-              ),
-            ),
-          ],
-        ),
-        TextButton(
-          onPressed: () => GlobalLauncher.launch(R.easyConnectDownloadUrl),
-          child: i18n.downloadEasyConnectBtn.txt,
-        ),
-        TextButton(
+                }
+              }),
+        ],
+      ),
+      /* TextButton(
           onPressed: () async {
             EasyLoading.show(status: '正在登录');
             final username = Kv.auth.currentUsername;
@@ -186,26 +179,22 @@ class _ConnectivityPageState extends State<ConnectivityPage> {
             }
           },
           child: const Text('一键登录i-SIT'),
-        ),
-      ],
-    );
+        ),*/
+    ];
   }
 
   @override
   Widget build(BuildContext context) {
+    final block =
+        isConnected ? buildConnectedBlock() : buildDisconnectedBlock();
     return Scaffold(
       appBar: AppBar(title: i18n.networkTool.txt),
-      body: Center(
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Expanded(flex: 3, child: buildFigure(context)),
-              // const Spacer(flex: 1),
-              Expanded(flex: 3, child: isConnected ? buildConnectedBlock() : buildDisconnectedBlock()),
-            ],
-          ),
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          buildFigure(context),
+          ...block,
+        ],
       ),
     );
   }
--- lib/module/connectivity/symbol.dart
+++ lib/module/connectivity/symbol.dart
@@ -16,4 +16,4 @@
  *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 export 'init.dart';
-export 'page/connectivity.dart';
+export 'page/index.dart';
