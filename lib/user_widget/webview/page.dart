5017ab4b01caed221fe96cbd67229f66b9b65e20
zzq <i@zhangzqs.cn>
2022-12-04 17:48:38 +0800

    Feat: WebView适配夜间模式

--- lib/user_widget/webview/page.dart
+++ lib/user_widget/webview/page.dart
@@ -19,10 +19,12 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:kite/design/utils.dart';
 import 'package:kite/l10n/extension.dart';
 import 'package:kite/launcher.dart';
 import 'package:kite/user_widget/webview/view.dart';
 import 'package:kite/util/logger.dart';
+import 'package:kite/util/rule.dart';
 import 'package:kite/util/url_launcher.dart';
 import 'package:universal_platform/universal_platform.dart';
 import 'package:webview_flutter/webview_flutter.dart';
@@ -73,6 +75,9 @@ class SimpleWebViewPage extends StatefulWidget {
   /// 自定义Action按钮
   final List<Widget>? otherActions;
 
+  /// 夜间模式
+  final bool followDarkMode;
+
   const SimpleWebViewPage({
     Key? key,
     required this.initialUrl,
@@ -90,6 +95,7 @@ class SimpleWebViewPage extends StatefulWidget {
     this.javascriptChannels,
     this.showLaunchButtonIfUnsupported = true,
     this.otherActions,
+    this.followDarkMode = true,
   }) : super(key: key);
 
   @override
@@ -179,7 +185,18 @@ class _SimpleWebViewPageState extends State<SimpleWebViewPage> {
               widget.onWebViewCreated!(controller);
             }
           },
-          injectJsRules: widget.injectJsRules,
+          injectJsRules: () {
+            if (!widget.followDarkMode) return null;
+            return [
+              if (Theme.of(context).isDark)
+                InjectJsRuleItem(
+                  rule: const ConstRule(true),
+                  asyncJavascript: rootBundle.loadString('assets/webview/dark.js'),
+                  injectTime: InjectJsTime.onPageStarted,
+                ),
+              if (widget.injectJsRules != null) ...widget.injectJsRules!,
+            ];
+          }(),
           onProgress: (value) {
             if (!mounted) return;
             setState(() => progress = value % 100);
